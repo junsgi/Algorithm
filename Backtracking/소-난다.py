2bbf1314705733c029dc3d7c3d6132f9b4ff0d3c
junsgi <qwer3932@gmail.com>
2023-09-03 22:03:59 +0900

    0903

--- Backtracking/소-난다.py
+++ Backtracking/소-난다.py
@@ -1,35 +1,33 @@
-check = [0] * 1001
-for i in range(4, 1001, 2): check[i] = 1
+check = [0] * 10001
+visit = [0] * 10001
+check[1] = 1
+for i in range(4, 10001, 2): check[i] = 1
 
-for i in range(3, 1001, 2):
+for i in range(3, 10001, 2):
     swi = 0
+    if check[i]: continue
     for j in range(2, i):
         if i * i < j or  i % j == 0: 
             swi = 1
             break
     if not swi:
-        for j in range(i + i, 1001, i):
+        for j in range(i + i, 10001, i):
             check[j] = 1
-
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-cow = [0] * m
-a.sort()
-result = set()
-def rec(depth, idx):
-    global n
+ans = []
+def rec(depth, idx, value):
+    global n, ans
     if depth == m:
-        ans = sum(cow)
-        if ans > 1000: return
-        if not check[ans]:
-            result.add(str(ans))
+        if not check[value] and not visit[value]:
+            visit[value] = 1
+            ans.append(value)
         return
     
     for i in range(idx, n):
-        cow[depth] = a[i]
-        rec(depth + 1, i + 1)
-rec(0, 0)
-if result:
-    print(" ".join(sorted(list(result))))
+        rec(depth + 1, i + 1, value + a[i])
+rec(0, 0, 0)
+if ans:
+    print(" ".join(map(str, sorted(ans))))
 else:
     print(-1)
