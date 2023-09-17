0788b689f93c4a2253e083a684189bc16394051d
junsgi <qwer3932@gmail.com>
2023-09-17 23:27:03 +0900

    0917

--- Bruteforcing/캐슬 디펜스.py
+++ Bruteforcing/캐슬 디펜스.py
@@ -1,24 +1,67 @@
 # https://www.acmicpc.net/problem/17135
 import sys
+import copy
+import heapq
 input = sys.stdin.readline
 n, m, d = map(int, input().split())
-enemy = []
-casle = []
+archer = [[-1, -1] for _ in range(3)]
 a = []
+enemy = []
+heap = []
+ans = 0
 for i in range(n):
     a.append(list(map(int, input().split())))
     for j in range(m):
         if a[i][j] == 1:
             enemy.append((i, j))
 
-"""
-1. 궁수는 3명 배치한다.
-2. 궁수가 공격하는 적은 
- - 거리가 D이하인 적 중에서 가장 가까운 적
- - 여럿일 경우 가장 왼쪽에 있는 적
- - (distance, y)
-3. 같은 적이 여러 궁수에게 공격당할 수 있다.
-4. 공격 받으면 제외
-5. 적은 아래로 한 칸 이동하며 n + 1에 도착하면 제외된다.
-6. 모든 적이 사라지면 게임 종료
-"""
+def START():
+    global n, m, d
+    a_copy = copy.deepcopy(a)
+    temp = []
+    cnt = 0
+    # 적 아래로 이동 (성이 한 칸 위로 올라감)
+    for ax in range(n, 0, -1):
+        temp.clear()
+        archer[0][0] = archer[1][0] = archer[2][0] = ax
+
+        # i번째 궁수가 가장 가까우면서 왼쪽에 있는 적 고름
+        for i in range(3):
+            heap.clear()
+
+            # 적 위치 확인 후 
+            for ei, ej in enemy:
+                distance = abs(ei - archer[i][0]) + abs(ej - archer[i][1])
+
+                # 궁수와 같은 행에 있거나 적이 있던곳이거나 사거리보다 멀리있으면 continue
+                if ei >= ax or a_copy[ei][ej] == 0 or distance > d: 
+                    continue
+                
+                heapq.heappush(heap, (distance, ej, ei))
+
+            if heap:
+                dis, y, x = heapq.heappop(heap)
+                temp.append((x, y))
+                
+        # 적 공격
+        for tx, ty in temp:
+            # 같은 적을 볼 수 있으므로 1일때만 count
+            if a_copy[tx][ty]: cnt += 1
+            a_copy[tx][ty] = 0
+
+    return cnt
+
+def BACK(depth, idx):
+    global n, m, ans
+    # 모든 궁수를 배치했다면
+    if depth == 3:
+        ans = max(ans, START())
+        return 
+    
+    for i in range(idx, m):
+        # 궁수 배치
+        archer[depth][1] = i
+        BACK(depth + 1, i + 1)
+
+BACK(0, 0)
+print(ans)
