e279ba8a84aca46ca1017bf1146dc4b35835d092
junsgi <qwer3932@gmail.com>
2023-09-22 15:55:36 +0900

    0922

---
+++ Greedy/강의실 배정.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/11000
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
--- Greedy/수 묶기.py
+++ Greedy/수 묶기.py
@@ -1,3 +1,4 @@
+# https://www.acmicpc.net/problem/1744
 import sys
 input = sys.stdin.readline
 n = int(input().strip())
