f137dd7791664211ad28247381739407ea1d7921
junsgi <qwer3932@gmail.com>
2023-09-26 00:21:36 +0900

    0926

--- Bruteforcing/체스판 다시 칠하기.py
+++ Bruteforcing/체스판 다시 칠하기.py
@@ -1,12 +1,27 @@
 # https://www.acmicpc.net/problem/1018
 import sys 
-from collections import deque
 input = sys.stdin.readline
 n, m = map(int, input().split())
-graph = [list(map(lambda x : 1 if x == 'W' else -1, input())) for _ in range(n)]
-s = 0
-ans = 0
-for i in graph:
-    s = sum(i)
-    ans += abs(s) - 1
-print(ans - 1)
+graph = [list(input().strip()) for _ in range(n)]
+ans = []
+
+for i in range(n - 7):
+    for j in range(m - 7):
+        Wdraw = 0
+        Bdraw = 0
+        # 8 x 8 정사각형 범위 설정
+        for x1 in range(i, i + 8):
+            for y1 in range(j, j + 8):
+                if (x1 + y1) % 2 == 0:
+                    if graph[x1][y1] != 'W':
+                        Wdraw += 1
+                    else:
+                        Bdraw += 1
+                else:
+                    if graph[x1][y1] != 'B':
+                        Wdraw += 1
+                    else:
+                        Bdraw += 1
+        ans.append(Bdraw)
+        ans.append(Wdraw)
+print(min(ans))
--- Implementation/낚시왕.py
+++ Implementation/낚시왕.py
@@ -2,17 +2,50 @@
 import sys
 input = sys.stdin.readline
 dire = [0, [-1, 0], [1, 0], [0, 1], [0, -1]]
-fishman = [0, 0]
+dire2 = [0, -1, 1, 1, -1]
 ans = 0
+def MOVE(sharks):
+    pass
+
+def EAT(sharks):
+    pass
+
 """
 r, c : 상어 위치
 s : 속력 (탐색 깊이)
 d : 이동방향
-z : 크기 (위치가 겹친다면 가장 큰 상어가 나머지 다 잡아먹음 (같으면 안먹는듯?))
+z : 크기 (위치가 겹친다면 가장 큰 상어가 나머지 다 잡아먹음)
 """
 N, M, S = map(int, input().split())
 graph = [[0] * (M + 1) for _ in range(N + 1)]
 sharks = []
 for _ in range(S):
     r, c, s, d, z = map(int, input().split())
-    sharks.append((r, c, s, d, z))
+    sharks.append((r, c, s, d, z, dire2[d]))
+
+for i in range(1, M + 1):
+    # 낚시왕이 오른쪽으로 한 칸 이동한다.
+
+    # 낚시왕이 있는 열에 있는 상어 중 가장 가까운 상어를 잡는다.
+    idx = -1
+    col = -1
+    for j in range(len(sharks)):
+        if sharks[j][1] == i:
+            if idx == -1 or col > sharks[j][0]:
+                col = sharks[j][0]
+                idx = j
+    if idx != -1:
+        ans += sharks[idx][4]
+        del sharks[idx]
+
+
+    # 상어가 이동한다.
+    # 모든 상어가 속력 만큼 이동
+    MOVE(sharks)
+
+    # 좌표가 겹친다면 크기가 큰 상어를 제외하고 모두 잡아먹는다.
+    EAT(sharks)
+
+    # continue
+    print(sharks)
+    exit()
