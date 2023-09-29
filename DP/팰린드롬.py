2393ad4a4b6ff07428dc4545169bc39bedaa1a93
junsgi <qwer3932@gmail.com>
2023-09-30 01:07:11 +0900

    0930

--- DP/팰린드롬.py
+++ DP/팰린드롬.py
@@ -13,9 +13,10 @@ for i in range(1, n):
         DP[i][i + 1] = 1
 
 for i in range(3, n + 1):
-    plus = 0
-    for j in range(1, n + 1):
-        if a[j + plus]
+    for j in range(1, n - i + 2):
+        if a[j] == a[j + i - 1] and DP[j + 1][j + i - 2] != 0:
+            DP[j][j + i - 1] = 1
+    
 m = int(input())
 for _ in range(m):
     x, y = map(int, input().split())
