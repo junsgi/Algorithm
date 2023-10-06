f6a9cd8c0b0c41e35897d966d44b4394100dd4c5
junsgi <qwer3932@gmail.com>
2023-10-06 20:59:02 +0900

    1006

---
+++ Implementation/주사위 굴리기.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/14499
+N, M, dx, dy, com = map(int, input().split())
+grp = [list(map(int, input().split())) for _ in range(N)]
+com = list(map(int, input().split()))
