9b1329cbf1938e1a9f40f7423c81665c1be1dd2a
junsgi <qwer3932@gmail.com>
2023-10-25 20:59:18 +0900

    1025

--- Dijkstra/달빛 여우____덜품.py
+++ Dijkstra/달빛 여우____덜품.py
@@ -4,36 +4,48 @@ from heapq import heappush, heappop
 input = sys.stdin.readline
 n, m = map(int, input().strip().split())
 graph = [[] for _ in range(n + 1)]
+heap = []
 for _ in range(m):
     s, e, c = map(int, input().strip().split())
-    graph[s].append([e, c])
-    graph[e].append([s, c])
+    graph[s].append([e, c * 2]) # 나누기 2를 하면서 생긴 부동 소수점 문제 해결
+    graph[e].append([s, c * 2])
 
-wolf = [[0x7fffffff] * (n + 1) for _ in range(2)]
-fox = [0x7fffffff] * (n + 1)
-fox[1] = wolf[1][1] = 0
-heap = []
-heappush(heap, (0, -1, 1))
-heappush(heap, (0, 1, 1))
+wolf = [[1e12] * (n + 1) for _ in range(2)]
+fox = [1e12] * (n + 1)
+
+heappush(heap, (0, 1))
+fox[1] = 0
 while heap:
-    cost, status, node  = heappop(heap)
-    if status == -1 and fox[node] < cost: continue
-    if status >= 0 and wolf[status][node] < cost: continue
-    for nx, tc in graph[node]:
-        if status == -1: # 여우면
-            if fox[nx] <= tc + cost: continue
-            fox[nx] = tc + cost
-            heappush(heap, (tc + cost, status, nx))
-        elif status == 1: # 빠른 늑대라면
-            if wolf[0][nx] <= cost + tc / 2: continue
-            wolf[0][nx] =  cost + tc / 2 
-            heappush(heap, (cost + tc / 2, 0, nx))
-        else: # 느린 늑대라면
-            if wolf[1][nx] <= cost + tc * 2: continue
-            wolf[1][nx] = cost + tc * 2
-            heappush(heap, (cost + tc * 2, 1, nx))
+    cst, nd = heappop(heap)
+    if fox[nd] < cst : continue
+    for nx, c in graph[nd]:
+        if fox[nx] <= c + cst: continue
+        fox[nx] = c + cst
+        heappush(heap, (c + cst, nx))
+
+# 0 느린상태
+# 1 빠른상태
+heappush(heap, (0, 1, 1))
+wolf[0][1] = 0
+while heap:
+    cst, nd, status = heappop(heap)
+    if status and wolf[0][nd] < cst: continue
+    if not status and wolf[1][nd] < cst : continue
+
+    for nx, c in graph[nd]:
+        if status and wolf[0][nx] >= c / 2 + cst:
+            wolf[0][nx] = c / 2 + cst
+            heappush(heap, (c / 2 + cst, nx, 0))
+        
+        if not status and wolf[1][nx] >= c * 2 + cst:
+            wolf[1][nx] = c * 2 + cst
+            heappush(heap, (c * 2 + cst, nx, 1))
 ans = 0
-for i in range(1, n + 1):
-    if fox[i] < wolf[1][i] and fox[i] < wolf[0][i]:
+for i in range(2, n + 1):
+   # if wolf[1][i] == 1e12 or wolf[0][i] == 1e12: continue
+    if fox[i] < min(wolf[1][i], wolf[0][i]):
         ans += 1
 print(ans)
+print(fox[1:])
+print(wolf[0][1:])
+print(wolf[1][1:])
---
+++ Dynamic Programming/1학년.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/5557
+n=int(input())
+arr=list(map(int,input().split()))
+DP = [[0] * 21 for _ in range(n)]
+DP[0][arr[0]]=1
+for i in range(1, n-1):
+  for j in range(21):
+    if DP[i - 1][j] != 0:
+      if j - arr[i] >= 0: DP[i][j - arr[i]] += DP[i - 1][j]
+      if j + arr[i] <= 20: DP[i][j + arr[i]] += DP[i - 1][j]
+print(DP[n - 2][arr[-1]])
