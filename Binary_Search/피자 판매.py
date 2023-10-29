409d728e28713228725a62ea9373301aacf42ad1
junsgi <qwer3932@gmail.com>
2023-10-30 00:41:44 +0900

    1030

--- Binary_Search/피자 판매.py
+++ Binary_Search/피자 판매.py
@@ -2,17 +2,3 @@
 P = int(input())
 n, m = map(int, input().split())
 AB = sorted([int(input()) for _ in range(n + m)])
-print(AB)
-p1 = 0
-p2 = (n + m) - 1
-ans = 0
-while p1 != p2:
-
-    mid = AB[p1] + AB[p2]
-    if mid == P: ans += 1
-    
-    if mid >= P:
-        p2 -= 1
-    else:
-        p1 += 1
-print(ans)
