526fedc87c5e69459c964f2433a75a18cc1f9c62
junsgi <qwer3932@gmail.com>
2023-11-01 02:04:09 +0900

    1101

--- BFS_DFS/나의 인생에는 수학과 함께.py
+++ BFS_DFS/나의 인생에는 수학과 함께.py
@@ -1,40 +1,40 @@
 # https://www.acmicpc.net/problem/17265
 n = int(input())
 arr = [list(input().split()) for _ in range(n)]
-DP = [[0] * n for _ in range(n)]
+visit = [[0] * n for _ in range(n)]
 def check(x, y):
     global n
-    return not (0 <= x < n and 0 <= y < n)
+    return 0 <= x < n and 0 <= y < n
 
 def oper(a, b, c):
     if b == '+' : return a + c
     if b == '-' : return a - c
     return a * c
 
-def DFS(x, y, ord, ort, value):
-    global MIN, MAX
-
-    if check(x, y + 1):
+def DFS(x, y, ord, ort):
+    global MIN, MAX, n
+    if x == n - 1 and y == n - 1 :
+        MIN = min(MIN, ord)
+        MAX = max(MAX, ord)
+        return
+    if check(x, y + 1) and not visit[x][y + 1]:
+        visit[x][y + 1] = 1
         if arr[x][y + 1].isdigit():
-            if ord:
-                DFS(x, y + 1, 0, "", value + oper(ord, ort, arr[x][y + 1]))
-            else:
-                DFS(x, y + 1, arr[x][y + 1], ort, value)
+            DFS(x, y + 1, oper(ord, ort, int(arr[x][y + 1])), ort)
         else:
-            DFS(x, y + 1, ord, arr[x][y + 1], value)
+            DFS(x, y + 1, ord, arr[x][y + 1])
+        visit[x][y + 1] = 0
 
-    if check(x + 1, y):
+    if check(x + 1, y) and not visit[x + 1][y]:
+        visit[x + 1][y] = 1
         if arr[x + 1][y].isdigit():
-            if ord:
-                DFS(x + 1, y, 0, "", value + oper(ord, ort, arr[x + 1][y]))
-            else:
-                DFS(x + 1, y, arr[x + 1][y], ort, value)
+            DFS(x + 1, y, oper(ord, ort, int(arr[x + 1][y])), ort)
         else:
-            DFS(x + 1, y, ord, arr[x + 1][y], value)
-
+            DFS(x + 1, y, ord, arr[x + 1][y])
+        visit[x + 1][y] = 0
 
 MIN = 0x7fffffff
-MAX = 0
-
-DFS(0, 0)
-print(MAX, MIN, 0, "", 0)
+MAX = -MIN
+visit[0][0] = 1
+DFS(0, 0, int(arr[0][0]), "")
+print(MAX, MIN)
--- Implementation/과제 진행하기.py
+++ Implementation/과제 진행하기.py
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/176962
+ # https://school.programmers.co.kr/learn/courses/30/lessons/176962
 def solution(plans):
     answer = []
     stk = []
@@ -7,35 +7,39 @@ def solution(plans):
         plans[i][1] = int(h) * 60 + int(s)
         plans[i][2] = int(plans[i][2])
     plans.sort(key = lambda x : x[1])
-    cur = plans[0][1]
-    work = plans[0]
-    i = 0
+
+    status = plans[0]
+    cTime = plans[0][1]
+
     for i in range(1, len(plans)):
+        endTime = cTime + status[2]
         # 인터럽트라면
-        if cur + work[2] > plans[i][1]:
-            work[2] = work[2] - (cur - plans[i][1])
-            stk.append(work)
-            work = plans[i]
-            cur = work[1]
-        # 새 작업을 시작할 수 있으면
-        elif cur + work[2] < plans[i][1]:
+        if endTime > plans[i][1]:
+            status[2] -= plans[i][1] - cTime 
+            stk.append(status)
+        # 작업이 마치는 동시에 시작한다면
+        elif endTime == plans[i][1]:
+            answer.append(status[0])
+        else:
+            answer.append(status[0])
             while stk:
                 value = stk.pop()
-                if cur + value[2] > plans[i][1]:
-                    value[2] = value[2] - (plans[i][1] - cur)
-                    stk.append(value)
-                    cur = plans[i][1]
-                    break
-                else:
+                if cTime + value[2] <= plans[i][1]:
                     answer.append(value[0])
-                    cur = value[1] + value[2]
-            work = plans[i]
-            cur = work[1]
-        else:
-            answer.append(work[0])
-            cur = plans[i][1]
-            work = plans[i]
-        
+                    cTime += value[2]
+                else:
+                    value[2] -= plans[i][1] - cTime
+                    stk.append(value)
+                    break
+
+        status = plans[i]
+        cTime = status[1]
+    if len(answer) != len(plans) :
+        stk.append(status)
+    while stk: answer.append(stk.pop()[0])
     return answer
 
-(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
+print(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
+print(solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]))
+print(solution([["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]))
+print(solution(	[["a", "09:00", "10"], ["b", "09:10", "10"], ["c", "09:15", "10"], ["d", "09:30", "10"], ["e", "09:35", "10"]]))
