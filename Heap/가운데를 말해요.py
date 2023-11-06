4c052a5253e917cdd6187a30dfbdc73f7c0cb841
junsgi <qwer3932@gmail.com>
2023-11-06 14:30:25 +0900

    1106

--- Heap/가운데를 말해요.py
+++ Heap/가운데를 말해요.py
@@ -2,40 +2,24 @@
 from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
-def get(cnt):
-    result = []
-    for _ in range(cnt):
-        num = heappop(heap)
-        result.append(num)
-    n = result[-1]
-    while result:
-        heappush(heap, result.pop())
-    return n
-heap = []
-
 n = int(input())
-x = int(input())
-print(x)
-heappush(heap, x)
-for _ in range(n - 1):
+MAX = []
+MIN = []
+mid = int(input())
+print(mid)
+for i in range(n - 1):
     x = int(input())
-    heappush(heap, x)
-    if len(heap) % 2:
-        print(get(len(heap) // 2 + 1))
+
+    t1, t2 = max(mid, x), min(mid, x)
+    if len(MAX) != len(MIN):
+        heappush(MIN, t1)
+        heappush(MAX, -t2)
+        if len(MAX) > len(MIN):
+            mid = -heappop(MAX)
+        else:
+            mid = heappop(MIN)
     else:
-        print(get(len(heap) // 2))
-
-
-"""
-left, mid, right
-1
-1, 0, 0
-
-5
-1, 5, 0
-1, 2, 5
-1, 2, 10
--99, 2, 10
-
-
-"""
+        heappush(MIN, t1)
+        heappush(MAX, -t2)
+        mid = -heappop(MAX)
+    print(mid)
