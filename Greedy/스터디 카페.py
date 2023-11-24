44911dc2e12d835f9a5b3517dfe958a70d5b5e44
junsgi <qwer3932@gmail.com>
2023-11-24 20:59:49 +0900

    1124

--- Greedy/스터디 카페.py
+++ Greedy/스터디 카페.py
@@ -1,41 +1,45 @@
 # https://www.acmicpc.net/problem/28284
 from collections import deque
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 cost = list(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(m)]
+LEN = 0
 cost.sort(reverse=True)
 arr.sort(key = lambda x : (x[0], x[1]))
+
 taskQue = deque()
-MAX = (arr[0][1] - arr[0][0] + 1) * cost[0]
-MIN = (arr[0][1] - arr[0][0] + 1) * cost[-1]
-taskQue.append(arr[0][1])
-cur = 0
-def solu():
-    global MAX, MIN
-    LEN = len(taskQue)
-    for _ in range(LEN):
-        pass
+MAX = 0
+MIN = 0
 
-for i in range(1, len(arr)):
-    st, ed = arr[i][0], arr[i][1]
-
-    # 작업중에 들어왔다면
-    if st <= taskQue[0]:
-        # 이전 작업 끝나기 전 끝난다면
-        if ed <= taskQue[0]:
-            MAX += (ed - st + 1) * cost[len(taskQue)]
-            MIN += (ed - st + 1) * cost[len(cost) - len(taskQue) - 1]
-        else:
-            MAX += (taskQue[0] - st + 1) * cost[len(taskQue)]
-            MIN += (taskQue[0] - st + 1) * cost[len(cost) - len(taskQue) - 1]
-            taskQue.append(ed)
-            ted = taskQue.popleft()
-            MAX += (ed - ted) * cost[len(taskQue) - 1]
-            MIN += (ed - ted) * cost[len(cost) - len(taskQue)]
-    else: # 앞 사람이 나갔다면
-        if taskQue:
-            taskQue.popleft()
-        MAX += (ed - st + 1) * cost[len(taskQue)]
-        MIN += (ed - st + 1) * cost[len(cost) - len(taskQue) - 1]
+for st, ed in arr:
+    # 사용중인 자리가 없거나 
+    # 이용 시간이 큐에 있는 
+    if not taskQue or taskQue[-1] < st:
+        taskQue.clear()
+        MAX += (ed - st + 1) * cost[0]
+        MIN += (ed - st + 1) * cost[-1]
         taskQue.append(ed)
+    else:
+        LEN = len(taskQue)
+        for _ in range(len(taskQue)):
+            usingEd = taskQue.popleft()
+            taskQue.append(usingEd)
+
+            if LEN > 0:
+                MAX += (usingEd - st + 1) * cost[LEN]
+                MIN += (ed - st + 1) * cost[LEN]
+
+                st = usingEd + 1
+            else:
+                MAX += (usingEd - st + 1) * cost[0]
+                MIN += (ed - st + 1) * cost[-1]
+
+                break
+            LEN -= 1
+        if st <= ed:
+            MAX += (ed - st + 1) * cost[0]
+            MIN += (ed - st + 1) * cost[-1]
+
 print(MIN, MAX)
