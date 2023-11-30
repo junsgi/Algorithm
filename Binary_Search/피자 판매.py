9ca104ddb85c9dfc48b2a74c0bb38b9bc264872f
junsgi <qwer3932@gmail.com>
2023-11-30 20:25:07 +0900

    1130

--- Binary_Search/피자 판매.py
+++ Binary_Search/피자 판매.py
@@ -1,8 +1,1 @@
 # https://www.acmicpc.net/problem/2632
-P = int(input())
-n, m = map(int, input().split())
-t = []
-for _ in range(n + m): t.append(int(input()))
-A = t[:n]
-B = sorted(t[n:])
-tA = [n]
