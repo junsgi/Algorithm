b55f8ec1d92adb4b9596b3e76c9d7c695084f9d5
junsgi <qwer3932@gmail.com>
2023-12-14 20:58:36 +0900

    1214

---
+++ BFS_DFS/트리의 순회.py
@@ -0,0 +1,38 @@
+# https://www.acmicpc.net/problem/2263
+import sys
+sys.setrecursionlimit(100_000)
+def DIV(node, start, end):
+    global n
+    if node in check or end - abs(start) <= 0:
+        return
+
+    print(node, end = ' ')
+    check.add(node)
+    
+    newLeftLength = inOrderInfo[node] - start
+    DIV(postOrder[newLeftLength - 1], start, newLeftLength)
+
+    DIV(postOrder[postOrderInfo[node] - 1], inOrderInfo[node], end)
+
+n = int(input())
+inOrderInfo = {}
+postOrderInfo = {}
+check = set()
+inOrder = list(map(int, input().split()))
+postOrder = list(map(int, input().split()))
+for i in range(n):
+    inOrderInfo[inOrder[i]] = i
+    postOrderInfo[postOrder[i]] = i
+root = postOrder[-1]
+DIV(root, 0, n)
+"""
+7
+3 2 5 4 1 6 7
+3 5 4 2 7 6 1
+
+1 2 3 4 5 6 7
+
+9
+2 1 4 6 3 9 7 5 8
+2 6 4 9 7 8 5 3 1
+"""
