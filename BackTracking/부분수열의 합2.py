3d083f51aad1478c026282ad7551664dcc2499e9
junsgi <qwer3932@gmail.com>
2023-12-19 23:19:27 +0900

    1219

--- BackTracking/부분수열의 합2.py
+++ BackTracking/부분수열의 합2.py
@@ -1,33 +1,18 @@
 # https://www.acmicpc.net/problem/1208
 n, target = map(int, input().split())
 arr = list(map(int, input().split()))
-temp = []
+A = []
+B = []
 check = set()
 answer = 0
-def BACK(idx, end, res, status):
+def BACK(idx, end, res, arr):
     global answer, n, target
     if idx == end:
-        if status:
-            temp.append(res)
-        elif not status:
-            le = 0
-            ri = len(temp) - 1
-            while le <= ri:
-                mid = (le + ri) // 2
-                t = temp[mid] + res
-                if t < target:
-                    le = mid + 1
-                elif target < t:
-                    ri = mid - 1
-                else:
-                    answer += 1
-                    break
         return
-    BACK(idx + 1, end, res + arr[idx], status)
-    BACK(idx + 1, end, res, status)
+    arr.append(res)
+    BACK(idx + 1, end, res + arr[idx], arr)
+    BACK(idx + 1, end, res, arr)
 
-BACK(0, n // 2, 0, True)
-temp.sort()
-BACK(n // 2, n, 0, False)
-if answer - 1 < 0: print(0)
-else: print(answer - 1)
+BACK(0, n // 2, 0, A)
+BACK(n // 2, n, 0, B)
+
