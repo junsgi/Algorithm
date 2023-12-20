f084a4bc9fb2a4346ddf255706c56e2b1610dd65
junsgi <qwer3932@gmail.com>
2023-12-20 20:55:09 +0900

    1220

--- Divide_And_Conquer/Z덜품.py
+++ Divide_And_Conquer/Z덜품.py
@@ -1,20 +1,23 @@
 # https://www.acmicpc.net/problem/1074
-import sys
-sys.setrecursionlimit(2 ** 14)
 n, r, c = map(int, input().split())
-swi = False
 answer = 0
-def rec(x, y, size):
-    global r, c, answer, swi
-    if x == r and y == c or swi: 
+def rec(x, y, size, length):
+    global r, c, answer
+    if x == r and y == c: 
         print(answer)
-        print(2 ** 15)
         exit()
     if size == 1:
-        answer += 1
         return
-    rec(x, y, size // 4)
-    rec(x, y + int(size ** 0.5) // 2, size // 4)
-    rec(x + int(size ** 0.5) // 2, y, size // 4)
-    rec(x + int(size ** 0.5) // 2, y + int(size ** 0.5) // 2, size // 4)
-rec(0, 0, 4 ** n)
+    tSize = length // 2
+    rec(x,         y,         size // 4)
+    rec(x,         y + tSize, size // 4)
+    rec(x + tSize, y,         size // 4)
+    rec(x + tSize, y + tSize, size // 4)
+rec(0, 0, 4 ** n, 2 ** n)
+"""
+한 변의 길이 (length) = 2 ** n 
+x + length // 2, y + length // 2
+x + length // 2, y + length
+x + length, y + length // 2
+x + length, y + length
+"""
