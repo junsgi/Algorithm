68b3c803f73ca7553ee7f83a8383eea06bdb30c5
junsgi <qwer3932@gmail.com>
2024-01-28 19:01:50 +0900

    0128

---
+++ Tree/개미.py
@@ -0,0 +1,53 @@
+import sys
+from collections import defaultdict, deque
+input = sys.stdin.readline
+
+def BFS():
+    que.append(1)
+    visit[1] = 1
+
+    while que:
+        node = que.popleft()
+        for v, c in graph[node]:
+            if visit[v] : continue
+            visit[v] = 1
+            DP[0][v] = node
+            WEIGHT[0][v] = c
+            que.append(v)
+            
+
+n = int(input())
+value = [0]
+graph = defaultdict(list)
+que = deque()
+DP = [[0] * (n + 1) for _ in range(n + 1)]
+WEIGHT = [[0] * (n + 1) for _ in range(n + 1)]
+visit = [0] * (n + 1)
+cnt = 0
+for _ in range(n):
+    value.append(int(input()))
+for _ in range(n - 1):
+    st, ed, c = map(int, input().strip().split())
+    graph[st].append([ed, c])
+    graph[ed].append([st, c])
+temp = 1
+while temp <= n:
+    cnt += 1
+    temp *= 2
+
+BFS()
+# 희소배열 생성
+for i in range(1, cnt + 1):
+    for j in range(1, n + 1):
+        DP[i][j] = DP[i - 1][DP[i - 1][j]]
+        WEIGHT[i][j] = WEIGHT[i - 1][j] + WEIGHT[i - 1][DP[i - 1][j]]
+
+for i in range(1, n + 1):
+    user = i
+    for j in range(cnt, -1, -1):
+        if WEIGHT[j][user] <= value[user]:
+            value[user] -= WEIGHT[j][user]
+            user = DP[j][user]
+    if not user: 
+        user += 1
+    print(user)
