e96e8741d43a9aec20f748baaf85383a2252ef58
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-19 20:56:10 +0900

    쉬운 계단 수.py 업데이트

--- Dynamic Programming/쉬운 계단 수.py
+++ Dynamic Programming/쉬운 계단 수.py
@@ -9,4 +9,4 @@ for i in range(2, n + 1):
     DP[i][9] = DP[i - 1][8]
     for j in range(1, 9):
         DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1_000_000_000
-print(sum(DP[n][1:]) % 1_000_000_000 )
+print(sum(DP[n][1:]) % 1_000_000_000)
