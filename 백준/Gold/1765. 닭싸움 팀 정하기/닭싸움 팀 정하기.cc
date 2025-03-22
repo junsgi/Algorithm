df87a2ae14a624bd0f87ee43b5074f161a245580
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-22 19:30:51 +0900

    [Gold II] Title: 닭싸움 팀 정하기, Time: 0 ms, Memory: 1256 KB -BaekjoonHub

--- 백준/Gold/1765. 닭싸움 팀 정하기/닭싸움 팀 정하기.cc
+++ 백준/Gold/1765. 닭싸움 팀 정하기/닭싸움 팀 정하기.cc
@@ -1,61 +1,49 @@
-/*
-내 원수들끼리는 서로 친한 친구임
-*/
-#pragma warning(disable:4996)
+// 내 원수끼리는 친구가 포인트
+// 원수 정보를 따로 저장하여 원수들끼리 UNION
 #include<stdio.h>
-#include<algorithm>
 #include<vector>
-#define M 6000
 using namespace std;
-int n, m, check[M], x, y, cnt[M], ans;
-vector<int> graph[M];
-vector<int> tmp;
-char s;
-int find(int node)
+int n, m, b, c;
+char a;
+int q[1001];
+vector<int> graph[1001];
+int find(int x)
 {
-	if (check[node] == node)
-		return check[node];
-	check[node] = find(check[node]);
-	return check[node];
+    if (q[x] == x) return x;
+    return q[x] = find(q[x]);
 }
 void Union(int x, int y)
 {
-	int fx = find(x);
-	int fy = find(y);
-
-	if (fx > fy)
-		check[fy] = fx;
-	else check[fx] = fy;
+    int fx = find(x), fy = find(y);
+    if (fx == fy) return;
+    if (fx < fy) q[fy] = fx;
+    else q[fx] = fy;
 }
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++) check[i] = i;
-	for (int i = 0; i < m; i++)
-	{
-		scanf(" %c%d%d", &s, &x, &y);
-		int flag = s == 'E' ? -1 : 1;
-		graph[x].push_back(y * flag); // 원수 사이면 음수로 표현
-		graph[y].push_back(x * flag);
-
-		if (s == 'F' && find(x) != find(y)) // 친한 친구들끼리는 그룹화 한다.
-			Union(x, y);
-	}
-	for (int i = 1; i <= n; i++) // 1번 친구부터 n번 친구까지
-	{
-		tmp.clear();
-		for (int j = 0; j < (int)graph[i].size(); j++) 
-		{
-			if (graph[i][j] < 0) // 원수 친구를 만났다면 일단 저장
-				tmp.push_back(-graph[i][j]);
-		}
-		
-		for (int j = 1; j < (int)tmp.size(); j++)
-			if (find(tmp[j - 1]) != find(tmp[j])) // 원수 친구들끼리는 친한 친구이니 그룹화한다.
-				Union(tmp[j - 1], tmp[j]);
-	}
-	for (int i = 1; i <= n; i++)
-		if (cnt[find(check[i])]++ == 0) ans++; // 그룹 세기
-	printf("%d", ans);
-	return 0;
+    int answer = 0;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    for (int i = 0; i <= n; i++)q[i] = i;
+    for (int i = 0; i < m; i++)
+    {
+        scanf(" %c%d%d", &a, &b, &c);
+        if (a == 'F')
+            Union(b, c);
+        else
+        {
+            graph[b].push_back(c);
+            graph[c].push_back(b);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (graph[i].size() < 2) continue;
+        for (int j = 1; j < graph[i].size(); j++)
+            Union(graph[i][j - 1], graph[i][j]);
+    }
+    for (int i = 1; i <= n; i++)
+        if (q[i] == i) answer++;
+    printf("%d", answer);
+    return 0;
 }
