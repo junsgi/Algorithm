e8977bb6d837b6feb1a9ba9a5d8f6699e10959fa
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-23 18:26:28 +0900

    [Gold II] Title: 문제집, Time: 44 ms, Memory: 3028 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -1,70 +1,69 @@
-#pragma warning(disable:4996)
 #include<stdio.h>
 #include<vector>
 using namespace std;
-int n, m, edge[32100], v, e;
-int heap[500000], len;
-vector<int> graph[32100];
+int n, m, check[32'001], tmp;
+vector<int> graph[32'001], heap;
+void up(int idx);
+void down(int idx);
+int main()
+{
+    scanf("%d%d", &n, &m);
+    heap.push_back(-1);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        check[b]++;
+        graph[a].push_back(b);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (check[i]) continue;
+        heap.push_back(i);
+        up(heap.size() - 1);
+    }
+    while (heap.size() != 1)
+    {
+        int node = heap[1];
+        printf("%d ", node);
+        heap[1] = heap.back();
+        heap.pop_back();
+        down(1);
+        for (int& tnode : graph[node])
+        {
+            if (check[tnode] == 1)
+            {
+                check[tnode] = 0;
+                heap.push_back(tnode);
+                up(heap.size() - 1);
+            }
+            else if (check[tnode] > 1)
+                check[tnode]--;
+        }
+    }
+    return 0;
+}
 void up(int idx)
 {
-	if (idx / 2 <= 0) return;
-	if (heap[idx / 2] > heap[idx])
-	{
-		int tmp = heap[idx];
-		heap[idx] = heap[idx / 2];
-		heap[idx / 2] = tmp;
-		up(idx / 2);
-	}
+    if (idx / 2 <= 0) return;
+    if (heap[idx] < heap[idx / 2])
+    {
+        tmp = heap[idx];
+        heap[idx] = heap[idx / 2];
+        heap[idx / 2] = tmp;
+        up(idx / 2);
+    }
 }
 void down(int idx)
 {
-	int child = idx * 2;
-	if (child > len) return;
-	if (child + 1 <= len && heap[child] > heap[child + 1])
-		child++;
-
-	if (heap[idx] > heap[child])
-	{
-		int tmp = heap[idx];
-		heap[idx] = heap[child];
-		heap[child] = tmp;
-		down(child);
-	}
-}
-int main()
-{
-	scanf("%d%d", &n, &m);
-	for (int i = 0; i < m; i++)
-	{
-		scanf("%d%d", &v, &e);
-		edge[e]++;
-		graph[v].push_back(e);
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		if (edge[i] == 0)
-		{
-			heap[++len] = i;
-			up(len);
-		}
-	}
-
-	while (len)
-	{
-		int node = heap[1];
-		printf("%d ", node);
-		heap[1] = heap[len--];
-		down(1);
-
-		for (int i = 0; i < (int)graph[node].size(); i++)
-		{
-			if (edge[graph[node][i]] == 0) continue;
-			if (--edge[graph[node][i]] == 0)
-			{
-				heap[++len] = graph[node][i];
-				up(len);
-			}
-		}
-	}
-	return 0;
+    int child = idx * 2;
+    if (child >= heap.size()) return;
+    if (child + 1 < heap.size() && heap[child] > heap[child + 1]) child++;
+    if (heap[idx] > heap[child])
+    {
+        tmp = heap[idx];
+        heap[idx] = heap[child];
+        heap[child] = tmp;
+        down(child);
+    }
 }
