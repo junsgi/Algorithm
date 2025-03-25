c72110161faf963cc9f5bc12d4c0d5379c5db186
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-25 16:52:48 +0900

    [Platinum V] Title: 임계경로, Time: 40 ms, Memory: 4196 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.cc
+++ 백준/Platinum/1948. 임계경로/임계경로.cc
@@ -1,108 +1,65 @@
-#pragma warning(disable:4996)
+// 위상 정렬로 만나는 시간을 구함 + (각 도시마다 마지막으로 도착한 시간을 구함)
+// BFS로 목적지에서 출발지로 쉬지 않고 달려야 하는 도로의 수를 구함
 #include<stdio.h>
 #include<vector>
-#include<queue>
+#define MAX(x,y)((x)<(y)?(y):(x))
 using namespace std;
-int n, m, edge[32100], ans[20000], t, v, e, st, ed, result = 0;
-int heap[500000], len, visit[20000];
-vector<pair<int, int>> graph[2][32100];
-int max(int a, int b) { return a < b ? b : a; }
-void up(int idx)
+struct Temp { int node, w; };
+int n, m, check[10'001], cost[10'001], st, ed, que[11111], front, rear;
+vector<Temp> graph[10'001], re[10'001];
+void init()
 {
-	if (idx / 2 <= 0) return;
-	if (heap[idx / 2] > heap[idx])
-	{
-		int tmp = heap[idx];
-		heap[idx] = heap[idx / 2];
-		heap[idx / 2] = tmp;
-		up(idx / 2);
-	}
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        graph[a].push_back({ b, c });
+        re[b].push_back({ a, c });
+        check[b]++;
+    }
+    scanf("%d%d", &st, &ed);
 }
-void down(int idx)
+void topologicalSort()
 {
-	int child = idx * 2;
-	if (child > len) return;
-	if (child + 1 <= len && heap[child] > heap[child + 1])
-		child++;
-
-	if (heap[idx] > heap[child])
-	{
-		int tmp = heap[idx];
-		heap[idx] = heap[child];
-		heap[child] = tmp;
-		down(child);
-	}
+    front = rear = -1;
+    que[++rear] = st;
+    while (front != rear)
+    {
+        int el = que[++front];
+        for (Temp& i : graph[el])
+        {
+            cost[i.node] = MAX(cost[i.node], cost[el] + i.w);
+            if (--check[i.node] == 0)
+                que[++rear] = i.node;
+        }
+    }
+    printf("%d\n", cost[ed]);
 }
-int BFS(int x)
+void bfs()
 {
-	int cnt = 0;
-	len = 0;
-	heap[++len] = x;
-	up(len);
-	visit[x] = 1;
-	while (len)
-	{
-		int node = heap[1];
-		heap[1] = heap[len--];
-		down(1);
-
-		for (int i = 0; i < (int)graph[1][node].size(); i++)
-		{
-			int tnode = graph[1][node][i].first;
-			int tcost = graph[1][node][i].second;
-			if (tcost + ans[tnode] == ans[node])
-			{
-				if (!visit[tnode])
-				{
-					visit[tnode] = 1;
-					heap[++len] = tnode;
-					up(len);
-				}
-				cnt++;
-			}
-		}
-
-	}
-	return cnt;
+    int cnt = 0;
+    front = rear = -1;
+    que[++rear] = ed;
+    check[ed] = 1;
+    while (front != rear)
+    {
+        int node = que[++front];
+        for (Temp& i : re[node])
+        {
+            if (cost[node] != cost[i.node] + i.w) continue;
+            cnt++;
+            if (check[i.node]) continue;
+            check[i.node] = 1;
+            que[++rear] = i.node;
+        }
+    }
+    printf("%d", cnt);
 }
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= m; i++)
-	{
-		scanf("%d%d%d", &v, &e, &t);
-		graph[0][v].push_back({ e, t });
-		graph[1][e].push_back({ v, t });
-		edge[e]++;
-	}
-	scanf("%d%d", &st, &ed);
-	for (int i = 1; i <= n; i++)
-	{
-		if (edge[i] == 0)
-		{
-			heap[++len] = i;
-			up(len);
-		}
-	}
-	while (len)
-	{
-		int node = heap[1];
-		heap[1] = heap[len--];
-		down(1);
-		for (int i = 0; i < (int)graph[0][node].size(); i++)
-		{
-			if (edge[graph[0][node][i].first] == 0) continue;
-
-			// 다음 노드를 짓는 비용과 현재 노드까지의 비용 + 다음 노드 짓는데 드는 비용을 비교
-			ans[graph[0][node][i].first] = max(ans[graph[0][node][i].first], ans[node] + graph[0][node][i].second);
-			if (--edge[graph[0][node][i].first] == 0)
-			{
-				heap[++len] = graph[0][node][i].first;
-				up(len);
-			}
-		}
-	}
-	printf("%d\n", ans[ed]);
-	printf("%d", BFS(ed));
-	return 0;
+    init();
+    topologicalSort();
+    bfs();
+    return 0;
 }
