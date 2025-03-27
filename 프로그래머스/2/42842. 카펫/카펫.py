030599a0cdf766076329ff901fdfb2b03018d9f8
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-27 17:27:26 +0900

    [level 2] Title: 카펫, Time: 101.39 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,15 +1,14 @@
+def p(n):
+    arr = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            arr.append(i)
+    return arr;
 def solution(brown, yellow):
-
-    카펫크기 = brown + yellow # 카펫 전체 크기
-    약수 = []
-
-    # 약수들로만 계산하기 위해 약수 구함
-    for i in range(3, 카펫크기 // 3 + 1): 
-        if 카펫크기 % i == 0:
-            약수.append(i)
-    print(약수)
-    for i in range(len(약수)):
-        세로 = 약수[i]
-        가로 = 카펫크기 // 세로
-        if (가로 - 2) * (세로 - 2) == yellow:
-            return [가로, 세로]
+    arr = p(brown + yellow)
+    for i in range(len(arr)):
+        width = arr[i]
+        height = arr[len(arr) - i - 1]
+        if width - 2 < 0: continue
+        if (width - 2) * (height - 2) == yellow:
+            return [height, width]
