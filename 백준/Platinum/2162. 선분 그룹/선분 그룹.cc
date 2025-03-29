75a06280671d608f2e34c706006367f05e5f1667
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-29 20:37:33 +0900

    [Platinum V] Title: 선분 그룹, Time: 88 ms, Memory: 1184 KB -BaekjoonHub

--- 백준/Platinum/2162. 선분 그룹/선분 그룹.cc
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.cc
@@ -1,80 +1,77 @@
+// CCW + Union Find
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
-#include<vector>
 using namespace std;
-int n, arr[3001], value[3001], ins[4][3001];
-int CCW(int a, int b, 
-		int c, int d, 
-		int e, int f)
+struct Temp { int x1, y1, x2, y2; };
+int n, p[3000], cnt[3000], ans, c;
+Temp arr[3000];
+int find(int x) 
 {
-	int t1 = a * d + c * f + e * b;
-	int t2 = c * b + e * d + a * f;
-	int res = t1 - t2;
-	if (res < 0) return -1;
-	if (res > 0) return 1;
-	return 0;
-}
-int find(int node)
-{
-	if (arr[node] == node)
-		return node;
-	return arr[node] = find(arr[node]);
+	if (p[x] == x) return x;
+	return p[x] = find(p[x]);
 }
 void Union(int x, int y)
 {
-	int fx = find(x);
-	int fy = find(y);
+	int fx = find(x), fy = find(y);
 	if (fx == fy) return;
 	if (fx < fy)
 	{
-		arr[fy] = fx;
-		value[fx] += value[fy];
+		p[fy] = fx;
+		cnt[fx] += cnt[fy];
+		cnt[fy] = 0;
 	}
 	else
 	{
-		arr[fx] = fy;
-		value[fy] += value[fx];
+		p[fx] = fy;
+		cnt[fy] += cnt[fx];
+		cnt[fx] = 0;
 	}
 }
-int check(int i, int j)
+int CCW(int a, int b, int c, int d,	int e, int f)
 {
-	int res1 = CCW(ins[0][i], ins[1][i], ins[2][i], ins[3][i], ins[0][j], ins[1][j]) *
-		CCW(ins[0][i], ins[1][i], ins[2][i], ins[3][i], ins[2][j], ins[3][j]);
-	int res2 = CCW(ins[0][j], ins[1][j], ins[2][j], ins[3][j], ins[0][i], ins[1][i]) *
-		CCW(ins[0][j], ins[1][j], ins[2][j], ins[3][j], ins[2][i], ins[3][i]);
-	if (res1 == 0 && res2 == 0)
-	{
-		return
-			min(ins[0][i], ins[2][i]) <= max(ins[0][j], ins[2][j]) &&
-			min(ins[0][j], ins[2][j]) <= max(ins[0][i], ins[2][i]) &&
-			min(ins[1][i], ins[3][i]) <= max(ins[1][j], ins[3][j]) &&
-			min(ins[1][j], ins[3][j]) <= max(ins[1][i], ins[3][i]);
-	}
-	if (res1 <= 0 && res2 <= 0) return 1;
+	int res = (a * d + c * f + e * b) - (c * b + e * d + a * f);
+	if (res < 0) return -1;
+	else if (res > 0) return 1;
+	return 0;
+}
+bool cmp(Temp& a, Temp& b)
+{
+	return min(a.x1, a.x2) <= max(b.x1, b.x2) && min(b.x1, b.x2) <= max(a.x1, a.x2) && min(a.y1, a.y2) <= max(b.y1, b.y2) && min(b.y1, b.y2) <= max(a.y1, a.y2);
+}
+int func(int i, int j)
+{
+	int i1 = CCW(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, arr[j].x1, arr[j].y1);
+	int i2 = CCW(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, arr[j].x2, arr[j].y2);
+	int j1 = CCW(arr[j].x1, arr[j].y1, arr[j].x2, arr[j].y2, arr[i].x1, arr[i].y1);
+	int j2 = CCW(arr[j].x1, arr[j].y1, arr[j].x2, arr[j].y2, arr[i].x2, arr[i].y2);
+
+	if (i1 * i2 == 0 && j1 * j2 == 0)
+		return cmp(arr[i], arr[j]);
+	else if (i1 * i2 <= 0 && j1 * j2 <= 0)
+		return 1;
 	return 0;
 }
 int main()
 {
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		arr[i] = i;
-		value[i] = 1;
-		scanf("%d%d%d%d", &ins[0][i], &ins[1][i], &ins[2][i], &ins[3][i]);
+		scanf("%d%d%d%d", &arr[i].x1, &arr[i].y1, &arr[i].x2, &arr[i].y2);
+		p[i] = i;
+		cnt[i] = 1;
 	}
-
-	for (int i = 1; i < n; i++)
-		for (int j = i + 1; j <= n; j++)
-			if (check(i, j))
+	for (int i = 0; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (func(i, j))
 				Union(i, j);
 
-	int ans = 0, group = 0;
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] == i) group++;
-		ans = max(ans, value[i]);
+		if (p[i] == i)
+			ans++;
+		c = c < cnt[i] ? cnt[i] : c;
 	}
-	printf("%d\n%d", group, ans);
+	printf("%d\n%d", ans, c);
 	return 0;
 }
