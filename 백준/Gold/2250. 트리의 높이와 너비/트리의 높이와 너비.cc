9b7b1323795e2d35b776bcacf2764e40eeeb5b49
junsgi <104434881+junsgi@users.noreply.github.com>
2025-03-30 10:06:54 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 4 ms, Memory: 1804 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
@@ -1,54 +1,47 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<vector>
-#include<string>
-#define MAX 0x7fffffff
 using namespace std;
-int n, cnt, ans1 = MAX, ans2, m, visit[10001];
-pair<int, int> check[10001];
-vector<int> arr[10001];
-int p, l, r;
-int max(int a, int b) { return a < b ? b : a; }
-int min(int a, int b) { return a > b ? b : a; }
+int n, level[10'001], check[10'001], c, answer = 1, m = 1;
+vector<int> graph[10'001];
 void inorder(int node, int depth)
 {
 	if (node == -1) return;
-	m = max(m, depth);
-	inorder(arr[node][0], depth + 1);
-	check[depth].first = min(check[depth].first, ++cnt);
-	check[depth].second = max(check[depth].second, cnt);
-	inorder(arr[node][1], depth + 1);
+	inorder(graph[node][0], depth + 1);
+	c++;
+	if (level[depth] == 0)
+		level[depth] = c;
+	else
+	{
+		if (m < c - level[depth] + 1 || (m == c - level[depth] + 1 && depth + 1 < answer))
+		{
+			m = c - level[depth] + 1;
+			answer = depth + 1;
+		}
+	}
+	inorder(graph[node][1], depth + 1);
+
 }
 int main()
 {
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		check[i] = {MAX, 0};
-		scanf("%d%d%d", &p, &l, &r);
-		arr[p].push_back(l);
-		arr[p].push_back(r);
-		if (l != -1) visit[l]++;
-		if (r != -1) visit[r]++;
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		graph[a].emplace_back(b);
+		graph[a].emplace_back(c);
+		check[b]++;
+		check[c]++;
 	}
-	int root = 0;
 	for (int i = 1; i <= n; i++)
 	{
-		if (visit[i] == 0)
+		if (!check[i])
 		{
-			root = i;
+			inorder(i, 0);
 			break;
 		}
 	}
-	inorder(root, 1);
-	for (int i = 1; i <= n; i++)
-	{
-		if (ans2 < check[i].second - check[i].first + 1)
-		{
-			ans1 = i;
-			ans2 = check[i].second - check[i].first + 1;
-		}
-	}
-	printf("%d %d", ans1, ans2);
+	printf("%d %d", answer, m);
 	return 0;
 }
