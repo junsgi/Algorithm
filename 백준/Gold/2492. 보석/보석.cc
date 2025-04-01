ee3a523587293e74214127f0e7c0179bad547097
junsgi <104434881+junsgi@users.noreply.github.com>
2025-04-01 14:58:34 +0900

    [Gold III] Title: 보석, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/2492. 보석/보석.cc
+++ 백준/Gold/2492. 보석/보석.cc
@@ -1,51 +1,30 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
-#include<vector>
 using namespace std;
-struct temp 
-{
-	int x, y;
-};
-vector<int> x, y;
-int n, m, t, k, a, b, ans, ax, ay;
-void sweeping()
-{
-	for (int i = 0; i < t; i++)
-	{
-		for (int j = 0; j < t; j++)
-		{
-			int x1 = x[i];
-			int y1 = y[j] - k;
-			int x2 = x1 + k;
-			int y2 = y[j];
-			int sum = 0;
-			for (int s = 0; s < t; s++)
-			{
-				if (x1 <= x[s] &&
-					x[s] <= x2 &&
-					y1 <= y[s] &&
-					y[s] <= y2) sum++;
-			}
-			if (sum > ans)
-			{
-				ans = sum;
-				ax = min(x1, n - k); // x좌표에서 k만큼 내려갔을 때 범위를 벗어난다면 n - k로 대입
-				ay = max(y2, k); //  y좌표에서 왼쪽으로 k만큼 이동했을 때 범위를 벗어난다면 k로 대입
-			}
-		}
-	}
-}
+int n, m, t, k, ox[100], oy[100], cnt, ax, ay;
 int main()
 {
-	scanf("%d%d%d%d", &n, &m, &t, &k);
-	for (int i = 0; i < t; i++)
-	{
-		scanf("%d%d", &a, &b);
-		x.push_back(a);
-		y.push_back(b);
-	}
-	sweeping();
-	printf("%d %d\n%d", ax, ay, ans);
-	return 0;
+    scanf("%d%d%d%d", &n, &m, &t, &k);
+    for (int i = 0; i < t; i++)
+        scanf("%d%d", &ox[i], &oy[i]);
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
+            int mx = ox[i] + k, my = oy[j];
+            int x = ox[i], y = oy[j] - k;
+            int c = 0;
+            for (int w = 0; w < t; w++)
+                c += (x <= ox[w] && ox[w] <= mx) && (y <= oy[w] && oy[w] <= my);
+            if (cnt < c)
+            {
+                ax = min(ox[i], n - k);
+                ay = max(oy[j], k);
+                cnt = c;
+            }
+        }
+    }
+    printf("%d %d\n%d", ax, ay, cnt);
+    return 0;
 }
