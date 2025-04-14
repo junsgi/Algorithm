f04d7b2770ae40b91b9b7186f71446bbd3d03b2a
junsgi <104434881+junsgi@users.noreply.github.com>
2025-04-14 18:34:38 +0900

    [Gold I] Title: 버스 갈아타기, Time: 220 ms, Memory: 1392 KB -BaekjoonHub

--- 백준/Gold/2536. 버스 갈아타기/버스 갈아타기.cc
+++ 백준/Gold/2536. 버스 갈아타기/버스 갈아타기.cc
@@ -1,90 +1,80 @@
-#pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
-#include<vector>
-#include<queue>
 using namespace std;
 typedef long long ll;
-struct temp {
-	int n, a, b, c, d;
-};
-int n, m, k, sx, sy, ex, ey, visit[5001];
-vector<temp> arr;
-queue<temp> que;
-int CCW(ll a, ll b, 
-		ll c, ll d, 
-		ll e, ll f)
+struct Loc {
+	ll x1, y1, x2, y2, no;
+}arr[5001];
+ll n, m, k, sx, sy, ex, ey, que[5111];
+int front, rear, visit[5001], path[5111];
+int ccw(ll a, ll b, ll c, ll d, ll e, ll f)
 {
-	long long t1 = (a * d + c * f + e * b);
-	long long t2 = (c * b + e * d + a * f);
-	long long res = t1 - t2;
-	if (res < 0) return -1;
+	ll res = (a * d + c * f + e * b) - (c * b + e * d + a * f);
 	if (res > 0) return 1;
+	else if (res < 0) return -1;
 	return 0;
 }
-int check(int a, int b, 
-		  int c, int d,	
-		  int e, int f, 
-		  int g, int h)
+void backtracking(int node)
 {
-	int res1 = CCW(a, b, c, d, e, f) * CCW(a, b, c, d, g, h);
-	int res2 = CCW(e, f, g, h, a, b) * CCW(e, f, g, h, c, d);
-	if (res1 == 0 && res2 == 0)
-	{
-		return
-			min(a, c) <= max(e, g) &&
-			min(e, g) <= max(a, c) &&
-			min(b, d) <= max(f, h) &&
-			min(f, h) <= max(b, d);
-	}
-	if (res1 <= 0 && res2 <= 0) return 1;
-	return 0;
-}
-int on(int a, int b, int c, int d, int e, int f)
-{
-	return
-		min(a, c) <= e &&
-		e <= max(a, c) &&
-		min(b, d) <= f &&
-		f <= max(b, d);
-}
-int BFS()
-{
-	for (int i = 0; i < (int)arr.size(); i++)
-	{
-		if (on(arr[i].a, arr[i].b, arr[i].c, arr[i].d, sx, sy))
-		{
-			que.push({ arr[i].n, arr[i].a, arr[i].b, arr[i].c, arr[i].d });
-			visit[arr[i].n] = 1;
-		}
-	}
-
-	while (!que.empty())
-	{
-		temp tmp = que.front(); que.pop();
-		if (on(tmp.a, tmp.b, tmp.c, tmp.d, ex, ey))
-			return visit[tmp.n];
-		for (int i = 0; i < (int)arr.size(); i++)
-		{
-			if (visit[arr[i].n]) continue;
-			if (!check(tmp.a, tmp.b, tmp.c, tmp.d, arr[i].a, arr[i].b, arr[i].c, arr[i].d)) continue;
-			visit[arr[i].n] = visit[tmp.n] + 1;
-			que.push({ arr[i].n, arr[i].a, arr[i].b, arr[i].c, arr[i].d });
-		}
-	}
-	return 0;
-
+	if (node == -1) return;
+	backtracking(path[node]);
+	printf("%lld ", arr[que[node]].no);
 }
 int main()
-{
-	scanf("%d%d%d", &n, &m, &k);
+{	
+	scanf("%lld%lld%lld", &m, &n, &k);
 	for (int i = 0; i < k; i++)
 	{
-		int  no, a, b, c, d;
-		scanf("%d%d%d%d%d", &no, &a, &b, &c, &d);
-		arr.push_back({ no, a, b , c , d });
+		ll a, b, c, d, e;
+		scanf("%lld%lld%lld%lld%lld", &a, &b, &c, &d, &e); 
+		a--; b--; c--; d--; e--;
+		arr[a] = { b, c, d, e, a };
 	}
-	scanf("%d%d%d%d", &sx, &sy, &ex, &ey);
-	printf("%d", BFS());
+	scanf("%lld%lld%lld%lld", &sx, &sy, &ex, &ey); sx--; sy--; ex--; ey--;
+
+	arr[5000] = { sx, sy, sx, sy, 5000 };
+	front = rear = -1;
+	que[++rear] = 5000;
+	visit[5000] = 1;
+	path[0] = -1;
+	while (front != rear)
+	{
+		Loc cur = arr[que[++front]];
+		int depth = visit[cur.no];
+		if (min(cur.x1, cur.x2) <= ex && ex <= max(cur.x1, cur.x2) && min(cur.y1, cur.y2) <= ey && ey <= max(cur.y1, cur.y2))
+		{
+			/*backtracking(front);*/
+			printf("%d", depth - 1);
+			return 0;
+		}
+		for (int i = 0; i < k; i++)
+		{
+			if (visit[arr[i].no]) continue;
+			int c1 = ccw(cur.x1, cur.y1, cur.x2, cur.y2, arr[i].x1, arr[i].y1);
+			int c2 = ccw(cur.x1, cur.y1, cur.x2, cur.y2, arr[i].x2, arr[i].y2);
+			int c3 = ccw(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, cur.x1, cur.y1);
+			int c4 = ccw(arr[i].x1, arr[i].y1, arr[i].x2, arr[i].y2, cur.x2, cur.y2);
+			if (c1 * c2 == 0 && c3 * c4 == 0) // 일직선이면
+			{
+				// 겹치는지 확인
+				int res = min(cur.x1, cur.x2) <= max(arr[i].x1, arr[i].x2) &&
+					min(arr[i].x1, arr[i].x2) <= max(cur.x1, cur.x2) &&
+					min(cur.y1, cur.y2) <= max(arr[i].y1, arr[i].y2) &&
+					min(arr[i].y1, arr[i].y2) <= max(cur.y1, cur.y2);
+
+				if (!res) continue;
+				que[++rear] = arr[i].no;
+				visit[arr[i].no] = depth + 1;
+				path[rear] = front; // 역추적
+
+			}
+			else if (c1 * c2 <= 0 && c3 * c4 <= 0) // 교차 판정 
+			{
+				que[++rear] = arr[i].no;
+				visit[arr[i].no] = depth + 1;
+				path[rear] = front; // 역추적
+			}
+		}
+	}
 	return 0;
 }
