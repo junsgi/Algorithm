e96da6413ec3a97ae3da8d09396cf62370b97ed8
junsgi <104434881+junsgi@users.noreply.github.com>
2025-04-15 15:19:15 +0900

    [Gold II] Title: 자동차경주, Time: 132 ms, Memory: 6508 KB -BaekjoonHub

--- 백준/Gold/2611. 자동차경주/자동차경주.cc
+++ 백준/Gold/2611. 자동차경주/자동차경주.cc
@@ -1,57 +1,44 @@
+#pragma warning(disable:4996)
 #include<stdio.h>
 #include<vector>
-#include<queue>
 using namespace std;
-vector<pair<int, int>> graph[1001];
-queue<pair<int, int>> que;
-int n, m, visit[1001], edge[1001], path[1001];
-void up(int idx);
-void down(int idx);
-void t(int node, int depth)
+int n, m, dp[1000], visit[1000];
+vector<pair<int, int>> graph[1000];
+int memo(int node, int depth)
 {
-	if (depth > 0 && node == 1)
-	{
-		printf("1 ");
-		return;
-	}
-	t(path[node], depth + 1);
-	printf("%d ", node);
+    if (node == 0 && depth > 0)
+        return 0;
+    if (dp[node] != 0) return dp[node];
+    for (pair<int, int>& t : graph[node])
+    {
+        int tnode = t.first, weight = t.second;
+        if (visit[tnode]) continue;
+        visit[tnode] = 1;
+        dp[node] = max(dp[node], memo(tnode, depth + 1) + weight);
+        visit[tnode] = 0;
+    }
+    return dp[node];
+}
+void print(int node)
+{
+    printf("%d ", node + 1);
+    for (pair<int, int>& t : graph[node])
+    {
+        if (dp[node] != dp[t.first] + t.second) continue;
+        print(t.first);
+    }
 }
 int main()
 {
-	scanf("%d%d", &n, &m);
-	fill(path, path + n + 1, -1);
-	for (int i = 0; i < m; i++)
-	{
-		int st, ed, cost;
-		scanf("%d%d%d", &st, &ed, &cost);
-		graph[st].push_back({ ed, cost });
-		edge[ed]++;
-	}
-	que.push({ 1, 0 });
-	while (!que.empty())
-	{
-		pair<int, int> tmp = que.front();
-		que.pop();
-		int node = tmp.first;
-		int cost = tmp.second;
-		if (node == 1 && cost > 0) continue;
-		for (int i = 0; i < (int)graph[node].size(); i++)
-		{
-			int tnode = graph[node][i].first;
-			int tcost = graph[node][i].second;
-
-			if (edge[tnode] == 0) continue;
-			if (visit[tnode] < tcost + cost)
-			{
-				visit[tnode] = tcost + cost;
-				path[tnode] = node;
-			}
-			if (--edge[tnode] == 0)
-				que.push({ tnode, visit[tnode]});
-		}
-	}
-	printf("%d\n", visit[1]);
-	t(1, 0);
-	return 0;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c); a--;b--;
+        graph[a].emplace_back(b, c);
+    }
+    printf("%d\n", memo(0, 0));
+    print(0);
+    printf("1");
+    return 0;
 }
