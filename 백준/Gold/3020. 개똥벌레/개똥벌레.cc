904b005dd20131c2a023f231e71baf89d6242f69
junsgi <104434881+junsgi@users.noreply.github.com>
2025-04-17 09:16:17 +0900

    [Gold V] Title: 개똥벌레, Time: 40 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.cc
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.cc
@@ -1,33 +1,35 @@
 #pragma warning(disable:4996)
-#include <algorithm>
-#include <queue>
-#include <string>
-#include <iostream>
-using namespace std;
-int n, h, cnt;
-int ps[500'200], MIN = 0x7fffffff;
+#include<stdio.h>
+long long n, m, arr[500'002], ans = 0x7fffffff, cnt;
 int main()
 {
-	cin >> n >> h;
+    scanf("%lld%lld", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        if ((i & 1) == 0)
+        {
+            arr[1]++;
+            arr[a + 1]--;
+        }
+        else
+        {
+            arr[m - a + 1]++;
+            arr[m + 1]--;
+        }
+    }
+    for (int i = 1; i <= m ;i++)
+    {
+        arr[i] += arr[i - 1];
+        if (arr[i] < ans)
+        {
+            cnt = 1;
+            ans = arr[i];
+        }
+        else if (arr[i] == ans)cnt++;
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		int N;
-		cin >> N;
-		ps[i % 2 ? 1 : h - N + 1]++;
-		ps[i % 2 ? N + 1 : h + 1]--;
-	}
-	for (int i = 1; i <= h; i++)
-	{
-		ps[i] += ps[i - 1];
-		if (ps[i] < MIN)
-		{
-			cnt = 1;
-			MIN = ps[i];
-		}
-		else if (ps[i] == MIN) cnt++;
-		
-	}
-	cout << MIN << " " << cnt;
-	return 0;
+    printf("%lld %lld", ans, cnt);
+    return 0;
 }
