185c35e2928c5594d65d3530857ec06ef1b404de
junsgi <104434881+junsgi@users.noreply.github.com>
2025-04-20 02:24:54 +0900

    [Platinum IV] Title: 집배원 한상덕, Time: 204 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.cc
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.cc
@@ -1,86 +1,107 @@
-#pragma warning(disable:4996)
-#include <stdio.h>
-#include <algorithm>
-#include <queue>
-#include <vector>
-#define M 1'000'000'000
+// 고도를 정렬 후 최소 최댓값을 정한다.
+// 탐색 경로의 고도가 최소 최댓값 사이를 유지하면서 모든 마을에 도착했다면 성공
+// 성공했다면 돌아오는길은 갔던 길로 돌아오면 되므로 구현x
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<string>
+#include<algorithm>
 using namespace std;
-typedef pair<int, int> pii;
-int n, sx, sy, height[50][50], box, visit[50][50], ans = M;
-int dire[2][8] = { {-1, -1, -1, 0, 1, 1, 1, 0}, {-1, 0, 1, 1, 1, 0, -1, -1} };
-int a = M, b = M;
-char map[50][50];
-queue<pii> que;
+struct Node {
+	int x, y;
+	Node(int tx, int ty) : x(tx), y(ty) {}
+};
+int n, cost[50][50], sx, sy, cnt, ans, visit[50][50], dire[8][2] = { {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1} };
+string graph[50];
+queue<Node> que;
 vector<int> arr;
-int BFS(int s, int e)
+void init()
 {
-	// 시작 지점의 높이가 범위에 속하지 않는다면 return 0
-	if (!(s <= height[sx][sy] && height[sx][sy] <= e))return 0;
-	fill(&visit[0][0], &visit[49][49], 0);
-	que = queue<pii>();
-	que.push({ sx, sy });
+	ans = 0x7fffffff;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> graph[i];
+		for (int j = 0; j < n; j++)
+		{
+			if (graph[i][j] == 'P')
+			{
+				sx = i;
+				sy = j;
+				graph[i][j] = '.';
+			}
+			if (graph[i][j] == 'K')
+				cnt++;
+		}
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cin >> cost[i][j];
+			arr.emplace_back(cost[i][j]);
+		}
+}
+
+int bfs(int a, int b)
+{
+	// 시작 위치가 범위에 들지 않으면 return
+	if (!(a <= cost[sx][sy] && cost[sx][sy] <= b)) return 0;
+	que = queue<Node>();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			visit[i][j] = 0;
+	int c = 0;
+	int MIN = cost[sx][sy];
+	int MAX = cost[sx][sy];
+	que.emplace(sx, sy);
 	visit[sx][sy] = 1;
-	int cnt = 0;
-	int MIN = M, MAX = 0;
+
 	while (!que.empty())
 	{
-		pii tmp = que.front(); que.pop();
-		int x = tmp.first;
-		int y = tmp.second;
-		MIN = min(MIN, height[x][y]);
-		MAX = max(MAX, height[x][y]);
-		for (int i = 0; i < 8; i++)
+		Node t = que.front(); que.pop();
+		MIN = min(MIN, cost[t.x][t.y]);
+		MAX = max(MAX, cost[t.x][t.y]);
+
+		for (const int(&i)[2] : dire)
 		{
-			int tx = x + dire[0][i];
-			int ty = y + dire[1][i];
-			if (!(0 <= tx && tx < n && 0 <= ty && ty < n)) continue;
-			if (visit[tx][ty]) continue;
-			if (!(s <= height[tx][ty] && height[tx][ty] <= e)) continue;
-			if (map[tx][ty] == 'K') cnt++;
-			MIN = min(MIN, height[tx][ty]);
-			MAX = max(MAX, height[tx][ty]);
-			if (cnt == box)
+			int tx = t.x + i[0], ty = t.y + i[1];
+			if (!(0 <= tx && tx < n && 0 <= ty && ty < n)) continue; // 범위
+			if (visit[tx][ty]) continue; // 방문
+			if (!(a <= cost[tx][ty] && cost[tx][ty] <= b)) continue; // 피로도
+			if (graph[tx][ty] == 'K') c++; // 마을 방문 횟수
+            MIN = min(MIN, cost[tx][ty]);
+			MAX = max(MAX, cost[tx][ty]);
+			if (c == cnt) // 모든 마을에 방문했다면 return 
 			{
 				ans = min(ans, MAX - MIN);
 				return 1;
 			}
-			que.push({ tx, ty });
 			visit[tx][ty] = 1;
+			que.emplace(tx, ty);
+
 		}
 	}
 	return 0;
 }
-int main()
+
+void solution()
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			scanf(" %c", &map[i][j]);
-			if (map[i][j] == 'P')
-			{
-				sx = i; sy = j;
-			}
-			if (map[i][j] == 'K') box++;
-		}
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			scanf("%d", &height[i][j]);
-			arr.push_back(height[i][j]);
-		}
-	}
 	sort(arr.begin(), arr.end());
-	int left = 0, right = 1, LEN = (int)arr.size();
-	while (left < LEN && right < LEN)
+	int left = 0, right = 1;
+	while (left <= right && right < arr.size())
 	{
-		// 모두 배달했다면 최소 최대간격을 줄인다.
-		if (BFS(arr[left], arr[right])) left++;
-		else right++;;
+		if (bfs(arr[left], arr[right]))
+			left++;
+		else
+			right++;
 	}
 	printf("%d", ans);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	init();
+	solution();
 	return 0;
 }
