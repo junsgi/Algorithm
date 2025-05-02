69fcd52f1f76455e7ccc60d5d4f7bbbe9a3078c3
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-02 19:31:58 +0900

    [Platinum V] Title: 거의 최단 경로, Time: 124 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Platinum/5719. 거의 최단 경로/거의 최단 경로.cc
+++ 백준/Platinum/5719. 거의 최단 경로/거의 최단 경로.cc
@@ -1,92 +1,82 @@
-/*
-시작 점에서 부터 모든 정점의 최단 거리를 구한다.
-도착지점에서부터
- (도착지점과 연결된 정점 까지의 최단거리 + 연결된 정점에서 도착 지점의 거리 ==  도착지점 최단거리 )
- 위 식이 성립되면 최단거리로 들어오는 간선이므로 간선을 잘라낸다
-
- BFS 알고리즘으로 모든 최단 거리 간선을 잘라낸다.
-*/
-
 #pragma warning(disable:4996)
-#include<stdio.h>
-#include<queue>
+// 거의 최단 경로란 최단 경로에 포함되지 않는 도로로만 이루어진 경로 중 가장 짧은 것
+#include<iostream>
 #include<vector>
+#include<queue>
+#include<algorithm>
+#define M 100000000
 using namespace std;
-int v, e, st, ed, graph[1'001][1'001], cost[1'000], visit[1'000];
-int MAX = 500'000'000;
+int n, m, s, e, cost[500], visit[500], matrix[500][500];
 queue<int> que;
-void dijkstra(int start)
-{
-	for (int i = 0; i < v; i++)
-	{
-		cost[i] = MAX;
-		visit[i]  = 0;
-	}
-	cost[start] = 0;
-	visit[start] = 1;
-	int node = start, tnode, min;
-	for (int i = 1; i < v; i++)
-	{
-		if (node == -1)
-			return;
-		min = MAX;
-		tnode = -1;
-		visit[node] = 1;
-		for (int j = 0; j < v; j++)
-		{
-			if (!visit[j])
-			{
-				if (cost[j] > cost[node] + graph[node][j])
-					cost[j] = cost[node] + graph[node][j];
 
-				if (min > cost[j])
-				{
-					min = cost[j];
-					tnode = j;
-				}
+
+void bfs()
+{
+	que = queue<int>();
+	que.emplace(e);
+	while (!que.empty())
+	{
+		int node = que.front(); que.pop();
+		visit[node] = 1;
+		for (int i = 0; i < n; i++)
+		{
+			if (i == node || cost[node] != cost[i] + matrix[i][node]) continue; // 비용을 확인하여 최단거리만 탐색한다.
+			matrix[i][node] = M; // 경로삭제
+			que.emplace(i);
+		}
+	}
+}
+
+void dijkstra()
+{
+	fill(cost, cost + n, M);
+	fill(visit, visit + n, 0);
+
+	cost[s] = 0;
+	int node = s, MIN = M, tnode = -1;
+	for (int i = 0; i < n; i++)
+	{
+		if (node == -1) break;
+		visit[node] = 1;
+		MIN = M;
+		tnode = -1;
+
+		for (int j = 0; j < n; j++)
+		{
+
+			if (!visit[j] && cost[node] + matrix[node][j] < cost[j])
+				cost[j] = cost[node] + matrix[node][j];
+
+			if (!visit[j] && cost[j] < MIN)
+			{
+				MIN = cost[j];
+				tnode = j;
 			}
 		}
 		node = tnode;
-	}
-}
-void BFS()
-{
-	que.push(ed);
-	while (!que.empty())
-	{
-		int tmp = que.front();
-		que.pop();
-
-		for (int i = 0; i < v; i++)
-		{
-			if (i != tmp && cost[i] + graph[i][tmp] == cost[tmp])
-			{
-				graph[i][tmp] = MAX;
-				que.push(i);
-			}
-		}
 	}
 }
 int main()
 {
 	while (1)
 	{
-		scanf("%d%d", &v, &e);
-		if (v + e == 0) break;
-		scanf("%d%d", &st, &ed);
-		for (int i = 0; i < v; i++)
-			for(int j = 0 ; j < v ; j++)
-				if (i != j)graph[i][j] = MAX;
-		for (int i = 0; i < e; i++)
+		cin >> n >> m;
+		if (n + m == 0) break;
+		fill(&matrix[0][0], &matrix[0][0] + 500 * 500, M);
+		for (int i = 0; i < n; i++) matrix[i][i] = 0;
+		cin >> s >> e;
+		for (int i = 0; i < m; i++)
 		{
 			int a, b, c;
-			scanf("%d%d%d", &a, &b, &c);
-			graph[a][b] = c;
+			cin >> a >> b >> c;
+			matrix[a][b] = c;
 		}
-		dijkstra(st);
-		BFS();
-		dijkstra(st);
-		printf("%d\n", cost[ed] >= MAX ? -1 : cost[ed]);
+		dijkstra(); // 최단경로를 구한다.
+		bfs(); // e부터 시작하여 최단경로를 삭제한다.
+		dijkstra(); // 거의 최단경로를 구한다.
+		if (cost[e] == M) cout << -1 << '\n';
+		else cout << cost[e] << '\n';
+
 	}
 	return 0;
 }
