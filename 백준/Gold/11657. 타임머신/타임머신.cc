43cc203fc752cc7a091d03df5ec5d8c75d990075
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-09 16:21:32 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 1208 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,47 +1,31 @@
-/*
-순회를 총 V - 1번하면 최단거리가 구해짐
-마지막으로 한 번 순회했을 때 값이 달라진다면 음수 사이클 존재
-
-*/
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
+#define M 50000000000ll
 using namespace std;
-struct temp {
-	long long st, ed, cost;
-}arr[6000];
-int n, m, a, b, c;
-long long visit[501], MAX = 1'000'000'000;
-int BellmanFord()
-{
-	int res = 0;
-	for (int i = 0; i < n; i++)
-	{
-		res = 1;
-		for (int j = 0; j < m; j++)
-		{
-			if (visit[arr[j].st] == MAX) continue;
-			if (visit[arr[j].ed] > visit[arr[j].st] + arr[j].cost)
-			{
-				visit[arr[j].ed] = visit[arr[j].st] + arr[j].cost;
-				res = 0;
-			}
-		}
-
-	}
-	return res;
-}
+int n, m, hit, x[6000], y[6000];
+long long cost[501], z[6000];
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 2; i <= n; i++) visit[i] = MAX;
-	for (int i = 0; i < m; i++)
-		scanf("%lld%lld%lld", &arr[i].st, &arr[i].ed, &arr[i].cost);
-	if (BellmanFord())
-	{
-		for (int i = 2; i <= n; i++)
-			printf("%lld\n", visit[i] == MAX ? -1 : visit[i]);
-	}
-	else printf("-1");
-	return 0;
+    scanf("%d%d", &n, &m);
+    fill(cost + 1, cost + n + 1, M);
+    for (int i = 0; i < m; i++)
+        scanf("%d%d%lld", &x[i], &y[i], &z[i]);
+    cost[1] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        hit = 0;
+        for (int j = 0; j < m; j++)
+        {
+            if (cost[x[j]] == M) continue;
+            if (cost[y[j]] <= cost[x[j]] + z[j]) continue;
+            hit = 1;
+            cost[y[j]] = cost[x[j]] + z[j];
+        }
+    }
+    if (hit) printf("-1");
+    else
+        for (int i = 2; i <= n; i++)
+            printf("%lld\n", cost[i] == M ? -1 : cost[i]);
+    return 0;
 }
