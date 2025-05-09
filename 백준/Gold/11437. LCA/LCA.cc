c33cfc6b3402ff69e761452c96f35148d19d4656
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-09 15:59:04 +0900

    [Gold III] Title: LCA, Time: 36 ms, Memory: 9784 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -1,63 +1,70 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
+#include<algorithm>
 #include<vector>
-#include<string>
-#define MAX 100'001
 using namespace std;
-int n, depth[MAX], pa[MAX];
-int max(int a, int b) { return a < b ? b : a; }
-int min(int a, int b) { return a > b ? b : a; }
-vector<int> graph[MAX];
-/*
-LCA
-1. 루트에서부터 모든 노드의 깊이를 구한다.
-2. 두 노드 사이의 깊이를 같게 한다.
-3. 두 노드가 서로 다르다면 각 노드의 부모로 이동한다.
-4. 두 노드가 다르다면 3번 과정을 반복
-5. 같다면 현재 값이 두 노드의 최소 공통 조상
-*/
-void DFS(int node)
+int n, m, ex, dp[18][50000], depth[50000];
+vector<int> graph[50000];
+void dfs(int node, int d)
 {
-	for (int i = 0; i < (int)graph[node].size(); i++)
-	{
-		if (depth[graph[node][i]] != -1) continue;
-		depth[graph[node][i]] = depth[node] + 1; // 깊이 저장
-		pa[graph[node][i]] = node; // 부모 저장
-		DFS(graph[node][i]);
-	}
+    depth[node] = d;
+    for (const int& tnode : graph[node])
+    {
+        if (depth[tnode]) continue;
+        dp[0][tnode] = node;
+        dfs(tnode, d + 1);
+    }
 }
 int main()
 {
-	scanf("%d", &n);
-	fill(depth, depth + n + 1, -1);
-	depth[1] = 0;
-	for (int i = 0; i < n - 1; i++)
-	{
-		int st, ed;
-		scanf("%d%d", &st, &ed);
-		graph[st].push_back(ed);
-		graph[ed].push_back(st);
-	}
-	DFS(1);
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		int n1, n2;
-		scanf("%d%d", &n1, &n2);
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i *= 2) ex++;
+    for (int i = 1; i < n; i++)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b); a--; b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
 
-		while (depth[n1] != depth[n2]) // 깊이가 같지 않다면 깊이를 맞춘다.
-		{
-			if (depth[n1] < depth[n2])
-				n2 = pa[n2];
-			else
-				n1 = pa[n1];
-		}
-		while (n1 != n2) // 서로 같지 않다면 조상 노드로 바꾼다.
-		{
-			n1 = pa[n1];
-			n2 = pa[n2];
-		}
-		printf("%d\n", n1);
-	}
-	return 0;
+    dfs(0, 1); // 각 노드의 깊이를 구함
+
+    // 희소배열 생성
+    for (int i = 1; i <= ex; i++)
+        for (int j = 0; j < n; j++)
+            dp[i][j] = dp[i - 1][dp[i - 1][j]];
+
+    scanf("%d", &m);
+    for(int i = 0 ; i < m ; i++)
+    {
+        int a, b, tmp;
+        scanf("%d%d", &a, &b); a--; b--;
+        if (depth[a] > depth[b])
+        {
+            tmp = a; a = b; b = tmp;
+        }
+        int diff = depth[b] - depth[a];
+
+        for (int i = ex; i >= 0; i--)
+        {
+            if ((diff & (1 << i)) == 0) continue;
+            b = dp[i][b];
+        }
+        while (a != b)
+        {
+            if (dp[0][a] == dp[0][b])
+            {
+                a = dp[0][a];
+                break;
+            }
+            for (int i = 0; i <= ex; i++)
+            {
+                if (dp[i][a] == dp[i][b]) continue; // 조상의 조상의,, 로 뛰어넘을 수 있으니 같다면 continue
+                a = dp[i][a];
+                b = dp[i][b];
+            }
+        }
+        printf("%d\n", a + 1);
+    }
+    return 0;
 }
