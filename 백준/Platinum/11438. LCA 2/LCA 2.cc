7f79361a9957aaaff49bb3c325f5b3fc3378dff4
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-09 16:01:05 +0900

    [Platinum V] Title: LCA 2, Time: 144 ms, Memory: 19376 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.cc
+++ 백준/Platinum/11438. LCA 2/LCA 2.cc
@@ -1,86 +1,70 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
+#include<algorithm>
 #include<vector>
-#include<queue>
-#define MAX 100'001
 using namespace std;
-int n, visit[MAX], DP[30][MAX], cnt;
-vector<int> graph[MAX];
-void BFS()
+int n, m, ex, dp[20][100000], depth[100000];
+vector<int> graph[100000];
+void dfs(int node, int d)
 {
-	queue<int> que;
-	que.push(1);
-	visit[1] = 1;
-
-	while (!que.empty())
-	{
-		int node = que.front();
-		que.pop();
-
-		for (int i = 0; i < (int)graph[node].size(); i++)
-		{
-			int tnode = graph[node][i];
-			if (visit[tnode]) continue;
-			visit[tnode] = visit[node] + 1;
-			DP[0][tnode] = node;
-			que.push(tnode);
-		}
-	}
+    depth[node] = d;
+    for (const int& tnode : graph[node])
+    {
+        if (depth[tnode]) continue;
+        dp[0][tnode] = node;
+        dfs(tnode, d + 1);
+    }
 }
 int main()
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n - 1; i++)
-	{
-		int st, ed;
-		scanf("%d%d", &st, &ed);
-		graph[st].push_back(ed);
-		graph[ed].push_back(st);
-	}
-	BFS();
-	for (int i = 1; i <= n; i *= 2) cnt++;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i *= 2) ex++;
+    for (int i = 1; i < n; i++)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b); a--; b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
 
-	for (int i = 1; i <= cnt; i++)
-		for (int j = 1; j <= n; j++)
-			DP[i][j] = DP[i - 1][DP[i - 1][j]];
+    dfs(0, 1); // 각 노드의 깊이를 구함
 
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		int n1, n2, t = 0;
-		scanf("%d%d", &n1, &n2);
-		if (visit[n1] < visit[n2])
-		{
-			t = n1; 
-			n1 = n2; 
-			n2 = t;
-		}
+    // 희소배열 생성
+    for (int i = 1; i <= ex; i++)
+        for (int j = 0; j < n; j++)
+            dp[i][j] = dp[i - 1][dp[i - 1][j]];
 
-		// n1 노드는 t만큼 올라가야됨
-		// 만약 t가 5일 때 이진수로 표현하면 101임
-		// 4번 1번 건너뛰면 됨 (logn)
-		t = visit[n1] - visit[n2];
-		for (int j = 0; j <= cnt; j++)
-		{
-			if (t & (1 << j))
-				n1 = DP[j][n1];
-		}
+    scanf("%d", &m);
+    for(int i = 0 ; i < m ; i++)
+    {
+        int a, b, tmp;
+        scanf("%d%d", &a, &b); a--; b--;
+        if (depth[a] > depth[b])
+        {
+            tmp = a; a = b; b = tmp;
+        }
+        int diff = depth[b] - depth[a];
 
-		if (n1 == n2)
-		{
-			printf("%d\n", n1);
-			continue;
-		}
-
-		for (int j = cnt; j >= 0; j--)
-		{
-			if (DP[j][n1] != DP[j][n2])
-			{
-				n1 = DP[j][n1];
-				n2 = DP[j][n2];
-			}
-		}
-		printf("%d\n", DP[0][n1]);
-	}
-	return 0;
+        for (int i = ex; i >= 0; i--)
+        {
+            if ((diff & (1 << i)) == 0) continue;
+            b = dp[i][b];
+        }
+        while (a != b)
+        {
+            if (dp[0][a] == dp[0][b])
+            {
+                a = dp[0][a];
+                break;
+            }
+            for (int i = 0; i <= ex; i++)
+            {
+                if (dp[i][a] == dp[i][b]) continue; // 조상의 조상의,, 로 뛰어넘을 수 있으니 같다면 continue
+                a = dp[i][a];
+                b = dp[i][b];
+            }
+        }
+        printf("%d\n", a + 1);
+    }
+    return 0;
 }
