8b7ff5cfe171cd83b65b3286ff66f6e45bf0f6b1
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-11 20:17:51 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 5152 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -1,69 +1,58 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#define M 500000000
 using namespace std;
-int n, m, graph[1'001][1'001], cost[1'000], visit[1'000], dire[1'000], start, end;
-int MAX = 0x7fffffff / 2, cnt = 1;
-
-void dijkstra()
+int n, m, k, s, e, cost[1001], graph[1001][1001], path[1000], visit[1001];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+int dfs(int node, int depth)
 {
-	for (int i = 1; i <= n; i++)
-		cost[i] = MAX;
-	cost[start] = 0;
-	int node = start, min = MAX;
-	int tnode = -1;
-	for (int i = 0; i < n - 1; i++)
-	{
-		if (node == -1) return;
-		min = MAX;
-		visit[node] = 1;
-		tnode = -1;
-		for (int j = 1; j <= n; j++)
-		{
-			// 바로 j로 가는게 더 비용이 적은지 현재 노드를 경유로해서 가는 것이 비용이 적게 드는지 확인
-			if (!visit[j] && cost[j] > cost[node] + graph[node][j])
-			{
-				cost[j] = cost[node] + graph[node][j];
-				dire[j] = node;
-			}
-			if (!visit[j] && min > cost[j])
-			{
-				min = cost[j];
-				tnode = j;
-			}
-		}
-		node = tnode;
-	}
-}
-
-void k(int idx, int depth)
-{
-	if (idx == start)
-	{
-		printf("%d\n", depth);
-		printf("%d ", start);
-		return;
-	}
-	k(dire[idx], depth + 1);
-	printf("%d ", idx);
+    path[k++] = node;
+    if (node == s) return depth;
+    visit[node] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (visit[i]) continue;
+        if (cost[node] != cost[i] + graph[i][node]) continue;
+        return dfs(i, depth + 1);
+    }
 }
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			if (i != j)graph[i][j] = MAX;
-
-	for (int i = 0; i < m; i++)
-	{
-		int st, ed, c;
-		scanf("%d%d%d", &st, &ed, &c);
-		if (graph[st][ed] > c)
-			graph[st][ed] = c;
-	}
-	scanf("%d%d", &start, & end);
-	dijkstra();
-
-	printf("%d\n", cost[end]);
-	k(end, 1);
-	return 0;
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) 
+    {
+        cost[i] = M;
+        for (int j = 1; j <= n; j++)
+            graph[i][j] = M;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        graph[a][b] = min(graph[a][b], c);
+    }
+    scanf("%d%d", &s, &e);
+    heap.push({ 0, s });
+    cost[s] = 0;
+    while (!heap.empty())
+    {
+        pair<int, int> c = heap.top(); heap.pop();
+        int node = c.second;
+        int weight = c.first;
+        if (cost[node] < weight) continue;
+        for(int i = 1 ; i <= n ; i++)
+        {
+            int tnode = i, tw = graph[node][tnode];
+            if (cost[tnode] <= cost[node] + tw) continue;
+            cost[tnode] = cost[node] + tw;
+            heap.push({ cost[tnode], tnode });
+        }
+    }
+    printf("%d\n%d\n", cost[e], dfs(e, 0) + 1);
+    for (int i = k - 1; i >= 0; i--)
+        printf("%d ", path[i]);
+    return 0;
 }
