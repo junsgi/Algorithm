0dab5850dee3fcd29a77c6c73736170a1f121e73
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-15 13:23:26 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -2,31 +2,27 @@
 #include<stdio.h>
 #include<algorithm>
 using namespace std;
-typedef long long ll;
-ll a1, b1, a2, b2, a3, b3, a4, b4;
-int CCW(ll a, ll b, ll c, ll d, ll e, ll f)
+long long a, b, c, d, e, f, g, h;
+int ccw(long long i, long long j,
+        long long k, long long x,
+        long long y, long long z)
 {
-	ll res = (a * d + c * f + e * b) - (c * b + e * d + a * f);
-	if (res < 0) return -1;
-	if (res > 0) return 1;
-	return 0;
+    long long res = (i * x + k * z + y * j) - (k * j + y * x + i * z);
+    if (res < 0) return -1;
+    else if (res > 0) return 1;
+    return 0;
 }
 int main()
 {
-	scanf("%lld%lld%lld%lld%lld%lld%lld%lld", &a1, &b1, &a2, &b2, &a3, &b3, &a4, &b4);
-	int res1 = CCW(a1, b1, a2, b2, a3, b3) * CCW(a1, b1, a2, b2, a4, b4);
-	int res2 = CCW(a3, b3, a4, b4, a1, b1) * CCW(a3, b3, a4, b4, a2, b2);
-	ll flag = 0;
-	if (res1 == 0 && res2 == 0)
-	{
-		flag =
-			min(a1, a2) <= max(a3, a4) &&
-			min(a3, a4) <= max(a1, a2) &&
-			min(b1, b2) <= max(b3, b4) &&
-			min(b3, b4) <= max(b1, b2);
-	}
-	else if (res1 <= 0 && res2 <= 0)
-		flag = 1;
-	printf("%lld", flag);
-	return 0;
+    scanf("%lld%lld%lld%lld%lld%lld%lld%lld", &a, &b, &c, &d, &e, &f, &g, &h);
+    int case1 = ccw(a, b, c, d, e, f) * ccw(a, b, c, d, g, h);
+    int case2 = ccw(e, f, g, h, a, b) * ccw(e, f, g, h, c, d);
+    int res = 0;
+    // (a, b), (c, d), (e, f), (g, h)
+    if (case1 == 0 && case2 == 0)
+        res = min(a, c) <= max(e, g) && min(e, g) <= max(a, c) && min(b, d) <= max(f, h) && min(f, h) <= max(b, d);
+    else if (case1 <= 0 && case2 <= 0)
+        res = 1;
+    printf("%d", res);
+    return 0;
 }
