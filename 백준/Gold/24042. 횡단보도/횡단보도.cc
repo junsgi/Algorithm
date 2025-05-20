5eedcb36c58c9608bed5a5f9868bdec7d54d260d
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-20 17:47:47 +0900

    [Gold I] Title: 횡단보도, Time: 444 ms, Memory: 29448 KB -BaekjoonHub

--- 백준/Gold/24042. 횡단보도/횡단보도.cc
+++ 백준/Gold/24042. 횡단보도/횡단보도.cc
@@ -1,98 +1,51 @@
 #pragma warning(disable:4996)
-#include <stdio.h>
-#include <algorithm>
-#include <vector>
-#define M 1'000'000'000'000L
+#include<stdio.h>
+#include<algorithm>
+#include<queue>
+#define M 500'000'000'000LL
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-struct temp {
-	int node; ll value;
+struct Node {
+    int node, state;
+    long long weight;
+    const bool operator<(const Node& x) const {
+        return weight > x.weight;
+    }
+    Node(int a, long long b, int c) : node(a), weight(b), state(c) {}
 };
-vector<pii> graph[100'001];
-vector<temp> heap;
-int n, m;
-ll check[100'001];
-void up(int idx);
-void down(int idx);
-
-ll dijkstra()
-{
-	heap.push_back({ 1, 0 });
-
-	while ((int)heap.size() != 1)
-	{
-		temp tmp = heap[1];
-		heap[1] = heap[(int)heap.size() - 1];
-		down(1);
-		heap.pop_back();
-
-		int node = tmp.node;
-		ll value = tmp.value;
-		if (node == n) continue;
-		if (check[node] < value) continue;
-		for (int i = 0; i < (int)graph[node].size(); i++)
-		{
-			int tnode = graph[node][i].first;
-			int tt = graph[node][i].second;
-
-			ll v = tt - value % m;
-			if (v < 0) v += m;
-
-			ll tvalue = value + v + 1;
-			if (check[tnode] < tvalue) continue;
-			check[tnode] = tvalue;
-			heap.push_back({ tnode, tvalue });
-			up((int)heap.size() - 1);
-		}
-	}
-	return check[n];
-}
-
+vector<pair<int, int>> graph[100001];
+int n, m, a, b;
+long long cost[100001];
+priority_queue<Node, vector<Node>> heap;
 int main()
 {
-	heap.push_back({});
-	scanf("%d%d", &n, &m);
-	fill(check + 2, check + n + 1, M);
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		scanf("%d%d", &a, &b);
-		graph[a].push_back({ b, i });
-		graph[b].push_back({ a, i });
-	}
-	printf("%lld", dijkstra());
-	return 0;
-}
-
-void change(int x, int y)
-{
-	temp t = heap[x];
-	heap[x] = heap[y];
-	heap[y] = t;
-}
-
-void up(int idx)
-{
-	int p = idx / 2;
-	if (!p) return;
-	if (heap[idx].value < heap[p].value)
-	{
-		change(idx, p);
-		up(p);
-	}
-}
-
-void down(int idx)
-{
-	int child = idx * 2;
-	if (child >= (int)heap.size()) return;
-	if (child + 1 < (int)heap.size() && heap[child].value > heap[child + 1].value)
-		child++;
-
-	if (heap[idx].value > heap[child].value)
-	{
-		change(idx, child);
-		down(child);
-	}
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d%d", &a, &b);
+        graph[a].push_back({ b, i });
+        graph[b].push_back({ a, i });
+    }
+    fill(cost + 1, cost + n + 1, M);
+    cost[1] = 0;
+    heap.emplace(1, 0, 0);
+    while (!heap.empty())
+    {
+        Node con = heap.top(); heap.pop();
+        if (cost[con.node] < con.weight) continue;
+        for (const pair<int, int>& next : graph[con.node])
+        {
+            int tcost = 0;
+            if (con.state <= next.second)
+                tcost = next.second - con.state + 1;
+            else
+                tcost = m - con.state + next.second + 1;
+            
+            if (cost[next.first] <= cost[con.node] + tcost) continue;
+            cost[next.first] = cost[con.node] + tcost;
+            if (next.first == n) continue;
+            heap.emplace(next.first, cost[next.first], (next.second + 1) % m);
+        }
+    }
+    printf("%lld", cost[n]);
+    return 0;
 }
