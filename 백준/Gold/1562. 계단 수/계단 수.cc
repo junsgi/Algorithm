002ee80001eab983b486f8c4b4d5e501db8bbabf
junsgi <104434881+junsgi@users.noreply.github.com>
2025-05-21 17:12:02 +0900

    [Gold I] Title: 계단 수, Time: 16 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,44 +1,35 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
-#define MOD 1'000'000'000
 using namespace std;
-typedef long long ll;
-int n;
-ll DP[101][10][1 << 10], ans;
+long long n, ans, dp[101][10][1 << 10], m = 1000000000;
 int main()
 {
-	scanf("%d", &n);
-	/*
-	숫자를 한 개만 사용하였을 때
-	1은 (0 ~ 9) 숫자 중 1만 사용
-	2는 (0 ~ 9) 숫자 중 2만 사용
-	...
-	9는 (0 ~ 9) 숫자 중 9만 사용
-	*/
-	for (int i = 1; i <= 9; i++)
-		DP[1][i][1 << i] = 1;
+    // 자릿수, 가장 앞에 위치한 수, 비트 연산을 통해 0부터 9의 존재
 
+    // 초기화
+    // 한 자릿수이면서 i로 시작하고 i를 포함하는 경우 추가
+    for (int i = 1; i < 10; i++) dp[1][i][1 << i] = 1;
 
-	for (int i = 2; i <= n; i++) // 사용할 숫자의 개수 (자릿수)
-	{
-		for (int j = 0; j <= 9; j++) // 가장 뒤에 오는 숫자
-		{
-			for (int k = 1; k < (1 << 10); k++) // 10개의 비트를 사용하거나 안 했을 때의 경우의 수
-			{
-				if (j != 9) 
-					DP[i][j][k | (1 << j)] += DP[i - 1][j + 1][k];
+    // dp
+    for (int i = 2; i <= 100; i++) // 자릿수
+    {
+        for (int j = 0; j < 10; j++) // 앞자리
+        {
+            for (int k = 1; k < (1 << 10); k++) // 0b0000000001 ~ 0b1111111111 경우의 수 모두 탐색
+            {
+                // k에 현자 앞자리수 j를 포함한 위치
+                if (j != 0) dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
+                if (j != 9) dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
+                dp[i][j][k | (1 << j)] %= m;
+            }
+        }
+    }
+    scanf("%lld", &n);
 
-				if (j != 0) 
-					DP[i][j][k | (1 << j)] += DP[i - 1][j - 1][k];
-
-				DP[i][j][k | (1 << j)] %= MOD;
-			}
-		}
-	}
-
-	for (int i = 0; i < 10; i++)
-		ans = (ans + DP[n][i][(1 << 10) - 1]) % MOD;
-	printf("%lld", ans % MOD);
-	return 0;
+    // n자릿수, i로 시작, (1 << 10) - 1 == 1023 == 0b1111111111
+    for (int i = 0; i < 10; i++)
+        ans = (ans + dp[n][i][(1 << 10) - 1]) % m; 
+    printf("%lld", ans);
+    return 0;
 }
