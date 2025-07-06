31ba2e212dcb2db2822b140f646512627e9385ee
junsgi <104434881+junsgi@users.noreply.github.com>
2025-07-06 23:31:30 +0900

    [Gold IV] Title: 두 로봇, Time: 384 ms, Memory: 143088 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/15971. 두 로봇/두 로봇.py
@@ -0,0 +1,25 @@
+#https://www.acmicpc.net/problem/15971
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, r1, r2 = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    st, ed, cost = map(int, input().split())
+    graph[st].append([ed, cost])
+    graph[ed].append([st, cost])
+visit = [0] * (n + 1)
+que = deque()
+visit[r1] = 1
+que.append([r1, 0, 0])
+answer = 0
+while que:
+    node, value, MAX = que.popleft()
+    if node == r2:
+        answer = value - MAX
+        break
+    for nx, nc in graph[node]:
+        if visit[nx]:continue
+        visit[nx] = 1
+        que.append([nx, value + nc, max(MAX, nc)])
+print(answer)
