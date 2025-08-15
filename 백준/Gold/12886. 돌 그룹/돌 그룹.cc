6e0f0cb77e60de94d2c94c530513ea7e9f6206fb
junsgi <104434881+junsgi@users.noreply.github.com>
2025-08-16 00:57:15 +0900

    [Gold IV] Title: 돌 그룹, Time: 16 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.cc
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -1,65 +1,61 @@
 #include <iostream>
-#include <unordered_set>
+#include <algorithm>
 #include <queue>
-#include <string>
 #define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 using namespace std;
 struct Node { int arr[3]; Node(int x, int y, int z) : arr{ x, y, z } {} };
-int a, b, c;
-unordered_set<string> check;
+int a, b, c, visit[1501][1501];
 queue<Node> que;
-string toString(int x, int y, int z) 
-{
-    int tmp[] = { z, y, x };
-    string res = "";
-    for (int& i : tmp)
-        while (i) { res = string(1, (i % 10 + '0')) + res; i /= 10; }
-    return res;
-}
 int main()
 {
     fastio;
     cin >> a >> b >> c;
     que.emplace(Node(a, b, c));
-    check.insert(toString(a, b, c));
     while (!que.empty())
     {
-        auto [arr] = que.front();
-        que.pop();
+        int* arr = que.front().arr;
         if (arr[0] == arr[1] && arr[1] == arr[2])
         {
             cout << 1;
             return 0;
         }
-        for (int i = 0; i < 2; i++)
+        sort(arr, arr + 3);
+        int tmp[3];
+        if (arr[0] != arr[1])
         {
-            for (int j = 1; j < 3; j++)
+            tmp[0] = arr[0] + arr[0];
+            tmp[1] = arr[1] - arr[0];
+            tmp[2] = arr[2];
+            if (!visit[tmp[0]][tmp[1]])
             {
-                if (arr[i] == arr[j]) continue;
-                int origini = arr[i], originj = arr[j];
-                int x = arr[i], y = arr[j];
-                if (x < y)
-                {
-                    y -= x;
-                    x += x;
-                }
-                else
-                {
-                    x -= y;
-                    y += y;
-                }
-                arr[i] = x;
-                arr[j] = y;
-                string ck = toString(arr[0], arr[1], arr[2]);
-                if (check.find(ck) == check.end())
-                {
-                    que.emplace(arr[0], arr[1], arr[2]);
-                    check.insert(ck);
-                }
-                arr[i] = origini;
-                arr[j] = originj;
+                visit[tmp[0]][tmp[1]] = 1;
+                que.emplace(Node(tmp[0], tmp[1], tmp[2]));
             }
         }
+        if (arr[0] != arr[2])
+        {
+            tmp[0] = arr[0] + arr[0];
+            tmp[1] = arr[1];
+            tmp[2] = arr[2] - arr[0];
+            if (!visit[tmp[0]][tmp[1]])
+            {
+                visit[tmp[0]][tmp[1]] = 1;
+                que.emplace(Node(tmp[0], tmp[1], tmp[2]));
+            }
+        }
+        if (arr[2] != arr[1])
+        {
+            tmp[0] = arr[0];
+            tmp[1] = arr[1] + arr[1];
+            tmp[2] = arr[2] - arr[1];
+            if (!visit[tmp[0]][tmp[1]])
+            {
+                visit[tmp[0]][tmp[1]] = 1;
+                que.emplace(Node(tmp[0], tmp[1], tmp[2]));
+            }
+        }
+        que.pop();
+
     }
     cout << 0;
     return 0;
